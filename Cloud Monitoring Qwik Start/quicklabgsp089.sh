0e8212e215b743e7f76cf7bf0dda40674532f72b
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-07-12 21:13:18 +0530

    Add files via upload

---
+++ Cloud Monitoring Qwik Start/quicklabgsp089.sh
@@ -0,0 +1,36 @@
+# Create the instance with the necessary metadata and tags
+gcloud compute instances create lamp-1-vm \
+    --project=$DEVSHELL_PROJECT_ID \
+    --zone=us-central1-a \
+    --machine-type=n1-standard-2 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --metadata=enable-oslogin=true \
+    --maintenance-policy=MIGRATE \
+    --provisioning-model=STANDARD \
+    --tags=http-server \
+    --create-disk=auto-delete=yes,boot=yes,device-name=lamp-1-vm,image=projects/debian-cloud/global/images/debian-10-buster-v20230629,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/us-central1-a/diskTypes/pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
+
+# Create firewall rule to allow incoming HTTP traffic on port 80
+gcloud compute firewall-rules create allow-http \
+    --project=$DEVSHELL_PROJECT_ID \
+    --direction=INGRESS \
+    --priority=1000 \
+    --network=default \
+    --action=ALLOW \
+    --rules=tcp:80 \
+    --source-ranges=0.0.0.0/0 \
+    --target-tags=http-server
+
+
+# Generate SSH keys
+gcloud compute config-ssh --project "$DEVSHELL_PROJECT_ID" --quiet
+
+# SSH into the instance and run commands
+gcloud compute ssh lamp-1-vm --project "$DEVSHELL_PROJECT_ID" --zone us-central1-a --command "sudo apt-get update && sudo apt-get install -y apache2 php7.0 && sudo service apache2 restart"
+
+
