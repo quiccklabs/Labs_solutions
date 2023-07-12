4ce1eece59885c7efdbdd5b1119c597c158dfa2c
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-07-12 21:52:42 +0530

    Update quicklabgsp089.sh

--- Cloud Monitoring Qwik Start/quicklabgsp089.sh
+++ Cloud Monitoring Qwik Start/quicklabgsp089.sh
@@ -34,3 +34,41 @@ gcloud compute config-ssh --project "$DEVSHELL_PROJECT_ID" --quiet
 gcloud compute ssh lamp-1-vm --project "$DEVSHELL_PROJECT_ID" --zone us-central1-a --command "sudo apt-get update && sudo apt-get install -y apache2 php7.0 && sudo service apache2 restart"
 
 
+
+
+
+
+cat > alert_config.json <<EOF
+{
+  "displayName": "Inbound Traffic Alert",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "VM Instance - Network traffic",
+      "conditionThreshold": {
+        "filter": "resource.type = \"gce_instance\" AND metric.type = \"agent.googleapis.com/interface/traffic\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "300s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_RATE"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "60s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": 500
+      }
+    }
+  ],
+  "alertStrategy": {
+    "autoClose": "604800s"
+  },
+  "combiner": "OR",
+  "enabled": true
+}
+EOF
+
+gcloud alpha monitoring policies create --policy-from-file=alert_config.json
