1b0b99385ed3f91694cf37db4825ae26d6291ee1
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-07-16 21:08:09 +0530

    Add files via upload

---
+++ Analyze Sentiment with Natural Language API Challenge Lab/quicklabarc130.sh
@@ -0,0 +1,40 @@
+
+
+
+cat > analyze-request.json <<EOF
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content": "Google, headquartered in Mountain View, unveiled the new Android phone at the Consumer Electronic Show.  Sundar Pichai said in his keynote that users love their new Android phones."
+  },
+  "encodingType": "UTF8"
+}
+EOF
+
+curl -s -H "Content-Type: application/json" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+"https://language.googleapis.com/v1/documents:analyzeSyntax" \
+-d @analyze-request.json > analyze-response.txt
+
+
+
+
+
+cat > multi-nl-request.json <<EOF
+{
+  "document": {
+    "type": "PLAIN_TEXT",
+    "content": "Le bureau japonais de Google est situé à Roppongi Hills, Tokyo.",
+    "language": "fr"
+  }
+}
+
+EOF
+
+
+curl -s -H "Content-Type: application/json" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+"https://language.googleapis.com/v1/documents:analyzeEntities" \
+-d @multi-nl-request.json > multi-response.txt
+
+
