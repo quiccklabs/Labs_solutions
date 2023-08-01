e6c63d6537c6c8f3c70200a62d8c3b0b3c972b6f
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-08-02 00:44:04 +0530

    Add files via upload

---
+++ Importing Data to a Firestore Database/quicklabgsp642.sh
@@ -0,0 +1,33 @@
+
+
+
+gcloud config set project $DEVSHELL_PROJECT_ID
+
+git clone https://github.com/rosera/pet-theory
+
+cd pet-theory/lab01
+
+npm install @google-cloud/firestore
+
+npm install @google-cloud/logging
+
+
+curl https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Importing%20Data%20to%20a%20Firestore%20Database/importTestData.js > importTestData.js
+
+npm install faker@5.5.3
+
+curl https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Importing%20Data%20to%20a%20Firestore%20Database/createTestData.js > createTestData.js 
+
+
+node createTestData 1000
+
+node importTestData customers_1000.csv
+
+npm install csv-parse
+
+node createTestData 20000
+node importTestData customers_20000.csv
+
+
+
+
