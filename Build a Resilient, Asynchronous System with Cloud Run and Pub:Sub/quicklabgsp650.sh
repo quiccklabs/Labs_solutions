156d78b0758c779a322efb9bdd90f10f485ac2ea
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-08-08 12:27:56 +0530

    Add files via upload

---
+++ Build a Resilient, Asynchronous System with Cloud Run and Pub:Sub/quicklabgsp650.sh
@@ -0,0 +1,280 @@
+
+
+
+
+
+gcloud pubsub topics create new-lab-report
+
+gcloud services enable run.googleapis.com
+
+git clone https://github.com/rosera/pet-theory.git
+
+cd pet-theory/lab05/lab-service
+
+npm install express
+npm install body-parser
+npm install @google-cloud/pubsub
+
+
+cat > package.json <<EOF_END
+{
+  "name": "lab05",
+  "version": "1.0.0",
+  "description": "This is lab05 of the Pet Theory labs",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "Patrick - IT",
+  "license": "ISC",
+  "dependencies": {
+    "@google-cloud/pubsub": "^4.0.0",
+    "body-parser": "^1.20.2",
+    "express": "^4.18.2"
+  }
+}
+EOF_END
+
+
+
+cat > index.js <<EOF_END
+const {PubSub} = require('@google-cloud/pubsub');
+const pubsub = new PubSub();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+app.post('/', async (req, res) => {
+  try {
+    const labReport = req.body;
+    await publishPubSubMessage(labReport);
+    res.status(204).send();
+  }
+  catch (ex) {
+    console.log(ex);
+    res.status(500).send(ex);
+  }
+})
+async function publishPubSubMessage(labReport) {
+  const buffer = Buffer.from(JSON.stringify(labReport));
+  await pubsub.topic('new-lab-report').publish(buffer);
+}
+EOF_END
+
+
+cat > Dockerfile <<EOF_END
+FROM node:10
+WORKDIR /usr/src/app
+COPY package.json package*.json ./
+RUN npm install --only=production
+COPY . .
+CMD [ "npm", "start" ]
+EOF_END
+
+
+
+
+
+
+cd ~/pet-theory/lab05/email-service
+
+npm install express
+npm install body-parser
+
+
+
+
+cat > package.json <<EOF_END
+{
+    "name": "lab05",
+    "version": "1.0.0",
+    "description": "This is lab05 of the Pet Theory labs",
+    "main": "index.js",
+    "scripts": {
+      "start": "node index.js",
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "keywords": [],
+    "author": "Patrick - IT",
+    "license": "ISC",
+    "dependencies": {
+      "body-parser": "^1.20.2",
+      "express": "^4.18.2"
+    }
+  }
+EOF_END
+
+
+cat > index.js <<EOF_END
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+app.post('/', async (req, res) => {
+  const labReport = decodeBase64Json(req.body.message.data);
+  try {
+    console.log(`Email Service: Report ${labReport.id} trying...`);
+    sendEmail();
+    console.log(`Email Service: Report ${labReport.id} success :-)`);
+    res.status(204).send();
+  }
+  catch (ex) {
+    console.log(`Email Service: Report ${labReport.id} failure: ${ex}`);
+    res.status(500).send();
+  }
+})
+function decodeBase64Json(data) {
+  return JSON.parse(Buffer.from(data, 'base64').toString());
+}
+function sendEmail() {
+  console.log('Sending email');
+}
+EOF_END
+
+
+cat > Dockerfile <<EOF_END
+FROM node:10
+WORKDIR /usr/src/app
+COPY package.json package*.json ./
+RUN npm install --only=production
+COPY . .
+CMD [ "npm", "start" ]
+EOF_END
+
+
+
+
+
+gcloud iam service-accounts create pubsub-cloud-run-invoker --display-name "PubSub Cloud Run Invoker"
+
+gcloud run services add-iam-policy-binding email-service --member=serviceAccount:pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com --role=roles/run.invoker --region $REGION --platform managed
+
+PROJECT_NUMBER=$(gcloud projects list --filter="qwiklabs-gcp" --format='value(PROJECT_NUMBER)')
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-pubsub.iam.gserviceaccount.com --role=roles/iam.serviceAccountTokenCreator
+
+EMAIL_SERVICE_URL=$(gcloud run services describe email-service --platform managed --region $REGION --format="value(status.address.url)")
+
+echo $EMAIL_SERVICE_URL
+
+gcloud pubsub subscriptions create email-service-sub --topic new-lab-report --push-endpoint=$EMAIL_SERVICE_URL --push-auth-service-account=pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com
+
+
+cd ~/pet-theory/lab05/sms-service
+
+npm install express
+npm install body-parser
+
+
+
+cat > package.json <<EOF_END
+{
+    "name": "lab05",
+    "version": "1.0.0",
+    "description": "This is lab05 of the Pet Theory labs",
+    "main": "index.js",
+    "scripts": {
+      "start": "node index.js",
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "keywords": [],
+    "author": "Patrick - IT",
+    "license": "ISC",
+    "dependencies": {
+      "body-parser": "^1.20.2",
+      "express": "^4.18.2"
+    }
+  }
+EOF_END
+
+
+
+cat > index.js <<EOF_END
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+app.post('/', async (req, res) => {
+  const labReport = decodeBase64Json(req.body.message.data);
+  try {
+    console.log(`SMS Service: Report ${labReport.id} trying...`);
+    sendSms();
+    console.log(`SMS Service: Report ${labReport.id} success :-)`);    
+    res.status(204).send();
+  }
+  catch (ex) {
+    console.log(`SMS Service: Report ${labReport.id} failure: ${ex}`);
+    res.status(500).send();
+  }
+})
+function decodeBase64Json(data) {
+  return JSON.parse(Buffer.from(data, 'base64').toString());
+}
+function sendSms() {
+  console.log('Sending SMS');
+}
+EOF_END
+
+
+cat > Dockerfile <<EOF_END
+FROM node:10
+WORKDIR /usr/src/app
+COPY package.json package*.json ./
+RUN npm install --only=production
+COPY . .
+CMD [ "npm", "start" ]
+EOF_END
+
+
+sleep 100
+
+
+gcloud builds submit \
+  --tag gcr.io/$GOOGLE_CLOUD_PROJECT/lab-report-service
+gcloud run deploy lab-report-service \
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/lab-report-service \
+  --platform managed \
+  --region $REGION \
+  --allow-unauthenticated \
+  --max-instances=1
+
+
+cat > deploy.sh <<EOF_END
+gcloud builds submit \
+  --tag gcr.io/$GOOGLE_CLOUD_PROJECT/email-service
+gcloud run deploy email-service \
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/email-service \
+  --platform managed \
+  --region $REGION \
+  --no-allow-unauthenticated \
+  --max-instances=1
+EOF_END
+
+chmod u+x deploy.sh
+
+./deploy.sh
+
+
+
+gcloud builds submit \
+  --tag gcr.io/$GOOGLE_CLOUD_PROJECT/sms-service
+gcloud run deploy sms-service \
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/sms-service \
+  --platform managed \
+  --region $REGION \
+  --no-allow-unauthenticated \
+  --max-instances=1
