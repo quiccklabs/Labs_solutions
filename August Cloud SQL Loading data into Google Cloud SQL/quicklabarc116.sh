e190eea2d578efc811273d85ddddb170b782dfcf
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-08-12 01:45:34 +0530

    Add files via upload

---
+++ August Cloud SQL Loading data into Google Cloud SQL/quicklabarc116.sh
@@ -0,0 +1,41 @@
+
+
+
+cat > $JSON_FILE_NAME <<EOF_END
+{
+	"item": {
+		"value": "Please update my records with the following information:\n Email address: foo@example.com,\nNational Provider Identifier: 1245319599"
+	},
+	"deidentifyConfig": {
+		"infoTypeTransformations": {
+			"transformations": [{
+				"primitiveTransformation": {
+					"replaceWithInfoTypeConfig": {}
+				}
+			}]
+		}
+	},
+	"inspectConfig": {
+		"infoTypes": [{
+				"name": "EMAIL_ADDRESS"
+			},
+			{
+				"name": "US_HEALTHCARE_NPI"
+			}
+		]
+	}
+}
+EOF_END
+
+
+curl -s \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Content-Type: application/json" \
+  https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/content:deidentify \
+  -d @$JSON_FILE_NAME -o $TXT_FILE_NAME
+
+
+  gsutil cp $TXT_FILE_NAME gs://$DEVSHELL_PROJECT_ID-redact
+
+
+
