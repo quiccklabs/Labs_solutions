196f319da9e9adaaa1c4b6cc082fa974783f8e38
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-08-22 00:29:36 +0530

    Add files via upload

---
+++ Interact with Terraform Modules/quicklabgsp751.sh
@@ -0,0 +1,243 @@
+
+
+git clone https://github.com/terraform-google-modules/terraform-google-network
+cd terraform-google-network
+git checkout tags/v6.0.1 -b v6.0.1
+
+gcloud config list --format 'value(core.project)'
+
+cd examples/simple_project
+
+cat > variables.tf <<EOF_END
+variable "project_id" {
+  description = "The project ID to host the network in"
+  default     = "$DEVSHELL_PROJECT_ID"
+}
+
+variable "network_name" {
+  description = "The name of the VPC network being created"
+  default     = "example-vpc"
+}
+EOF_END
+
+terraform init
+
+terraform apply --auto-approve
+
+terraform destroy --auto-approve
+
+rm -rd terraform-google-network -f
+
+
+#task 2
+
+cd ~
+touch main.tf
+mkdir -p modules/gcs-static-website-bucket
+
+cd modules/gcs-static-website-bucket
+touch website.tf variables.tf outputs.tf
+
+tee -a README.md <<EOF
+# GCS static website bucket
+This module provisions Cloud Storage buckets configured for static website hosting.
+EOF
+
+tee -a LICENSE <<EOF
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+EOF
+
+
+cat > website.tf <<EOF_END
+resource "google_storage_bucket" "bucket" {
+  name               = var.name
+  project            = var.project_id
+  location           = var.location
+  storage_class      = var.storage_class
+  labels             = var.labels
+  force_destroy      = var.force_destroy
+  uniform_bucket_level_access = true
+  versioning {
+    enabled = var.versioning
+  }
+  dynamic "retention_policy" {
+    for_each = var.retention_policy == null ? [] : [var.retention_policy]
+    content {
+      is_locked        = var.retention_policy.is_locked
+      retention_period = var.retention_policy.retention_period
+    }
+  }
+  dynamic "encryption" {
+    for_each = var.encryption == null ? [] : [var.encryption]
+    content {
+      default_kms_key_name = var.encryption.default_kms_key_name
+    }
+  }
+  dynamic "lifecycle_rule" {
+    for_each = var.lifecycle_rules
+    content {
+      action {
+        type          = lifecycle_rule.value.action.type
+        storage_class = lookup(lifecycle_rule.value.action, "storage_class", null)
+      }
+      condition {
+        age                   = lookup(lifecycle_rule.value.condition, "age", null)
+        created_before        = lookup(lifecycle_rule.value.condition, "created_before", null)
+        with_state            = lookup(lifecycle_rule.value.condition, "with_state", null)
+        matches_storage_class = lookup(lifecycle_rule.value.condition, "matches_storage_class", null)
+        num_newer_versions    = lookup(lifecycle_rule.value.condition, "num_newer_versions", null)
+      }
+    }
+  }
+}
+EOF_END
+
+
+cat > variables.tf <<EOF_END
+variable "name" {
+  description = "The name of the bucket."
+  type        = string
+}
+variable "project_id" {
+  description = "The ID of the project to create the bucket in."
+  type        = string
+}
+variable "location" {
+  description = "The location of the bucket."
+  type        = string
+}
+variable "storage_class" {
+  description = "The Storage Class of the new bucket."
+  type        = string
+  default     = null
+}
+variable "labels" {
+  description = "A set of key/value label pairs to assign to the bucket."
+  type        = map(string)
+  default     = null
+}
+variable "bucket_policy_only" {
+  description = "Enables Bucket Policy Only access to a bucket."
+  type        = bool
+  default     = true
+}
+variable "versioning" {
+  description = "While set to true, versioning is fully enabled for this bucket."
+  type        = bool
+  default     = true
+}
+variable "force_destroy" {
+  description = "When deleting a bucket, this boolean option will delete all contained objects. If false, Terraform will fail to delete buckets which contain objects."
+  type        = bool
+  default     = true
+}
+variable "iam_members" {
+  description = "The list of IAM members to grant permissions on the bucket."
+  type = list(object({
+    role   = string
+    member = string
+  }))
+  default = []
+}
+variable "retention_policy" {
+  description = "Configuration of the bucket's data retention policy for how long objects in the bucket should be retained."
+  type = object({
+    is_locked        = bool
+    retention_period = number
+  })
+  default = null
+}
+variable "encryption" {
+  description = "A Cloud KMS key that will be used to encrypt objects inserted into this bucket"
+  type = object({
+    default_kms_key_name = string
+  })
+  default = null
+}
+variable "lifecycle_rules" {
+  description = "The bucket's Lifecycle Rules configuration."
+  type = list(object({
+    # Object with keys:
+    # - type - The type of the action of this Lifecycle Rule. Supported values: Delete and SetStorageClass.
+    # - storage_class - (Required if action type is SetStorageClass) The target Storage Class of objects affected by this Lifecycle Rule.
+    action = any
+    # Object with keys:
+    # - age - (Optional) Minimum age of an object in days to satisfy this condition.
+    # - created_before - (Optional) Creation date of an object in RFC 3339 (e.g. 2017-06-13) to satisfy this condition.
+    # - with_state - (Optional) Match to live and/or archived objects. Supported values include: "LIVE", "ARCHIVED", "ANY".
+    # - matches_storage_class - (Optional) Storage Class of objects to satisfy this condition. Supported values include: MULTI_REGIONAL, REGIONAL, NEARLINE, COLDLINE, STANDARD, DURABLE_REDUCED_AVAILABILITY.
+    # - num_newer_versions - (Optional) Relevant only for versioned objects. The number of newer versions of an object to satisfy this condition.
+    condition = any
+  }))
+  default = []
+}
+EOF_END
+
+
+
+cat > outputs.tf <<EOF_END
+output "bucket" {
+  description = "The created storage bucket"
+  value       = google_storage_bucket.bucket
+}
+EOF_END
+
+cd ~
+
+cat > main.tf <<EOF_END
+module "gcs-static-website-bucket" {
+  source = "./modules/gcs-static-website-bucket"
+  name       = var.name
+  project_id = var.project_id
+  location   = "$REGION"
+  lifecycle_rules = [{
+    action = {
+      type = "Delete"
+    }
+    condition = {
+      age        = 365
+      with_state = "ANY"
+    }
+  }]
+}
+EOF_END
+
+
+cat > outputs.tf <<EOF_END
+output "bucket-name" {
+  description = "Bucket names."
+  value       = "module.gcs-static-website-bucket.bucket"
+}
+EOF_END
+
+
+cat > variables.tf <<EOF_END
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "$DEVSHELL_PROJECT_ID"
+}
+variable "name" {
+  description = "Name of the buckets to create."
+  type        = string
+  default     = "$DEVSHELL_PROJECT_ID"
+}
+EOF_END
+
+terraform init
+
+terraform apply --auto-approve
+
+cd ~
+curl https://raw.githubusercontent.com/hashicorp/learn-terraform-modules/master/modules/aws-s3-static-website-bucket/www/index.html > index.html
+curl https://raw.githubusercontent.com/hashicorp/learn-terraform-modules/blob/master/modules/aws-s3-static-website-bucket/www/error.html > error.html
+
+gsutil cp *.html gs://$DEVSHELL_PROJECT_ID
