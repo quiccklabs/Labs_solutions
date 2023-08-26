ad8d8a7a49199af50f00b688ec94ccb8c9e82f06
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-08-26 22:09:52 +0530

    Add files via upload

---
+++ Activity Add and manage users with Linux commands/quicklab.sh
@@ -0,0 +1,16 @@
+
+
+
+
+
+sudo useradd researcher9
+
+sudo usermod -aG research_team researcher9
+
+sudo chown researcher9:research_team /home/researcher2/projects/project_r.txt
+
+sudo usermod -aG sales_team researcher9
+
+sudo userdel researcher9
+
+
