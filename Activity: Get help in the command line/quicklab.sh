4a2ae5cda106b8ebfe96388227317be32f866c15
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-09-03 21:40:35 +0530

    Create quicklab.sh

---
+++ Activity: Get help in the command line/quicklab.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+whatis cat
+
+man cat
+
+apropos -a first part file
+
+apropos -a first part file
+
+MANPAGER="cat" man useradd
+
+whatis rm
+
+whatis rmdir
+
+apropos -a "create new group"
+
+apropos -a "create new group"
