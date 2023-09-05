147b8ad66c1b3cffe5dd4a7b1d572adb52b53eee
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-09-05 20:50:04 +0530

    Add files via upload

---
+++ Rent a VM to Process Earthquake Data/quicklab.sh
@@ -0,0 +1,32 @@
+
+
+#!/bin/bash
+
+# Update the package list
+sudo apt-get update
+
+# Install git
+sudo apt-get -y -qq install git
+
+# Install python-mpltoolkits.basemap
+sudo apt-get install python-mpltoolkits.basemap -y
+
+# Check Git version
+git --version
+
+# Clone the repository
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+
+# Navigate to the appropriate directory
+cd training-data-analyst/CPB100/lab2b
+
+# Run the necessary scripts
+bash ingest.sh
+bash install_missing.sh
+python3 transform.py
+
+# List the files in the directory
+ls -l
+
+# Copy files to Google Cloud Storage
+gsutil cp earthquakes.* gs://$DEVSHELL_PROJECT_ID/earthquakes/
