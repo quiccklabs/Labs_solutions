6ccff056cdf953f648761ae5fa3acdda09846bcd
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-09-06 22:18:46 +0530

    Add files via upload

---
+++ Vertex AI PaLM API Qwik Start/quicklabgsp1155.sh
@@ -0,0 +1,307 @@
+
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+#export ZONE=us-central1-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
+
+gcloud services enable aiplatform.googleapis.com
+
+sleep 60
+
+echo "${GREEN}${BOLD}
+
+Task 1 Completed
+
+${RESET}"
+
+
+#task 2
+
+MODEL_ID="text-bison"
+PROJECT_ID=$DEVSHELL_PROJECT_ID
+curl \
+-X POST \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H "Content-Type: application/json" \
+https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:predict -d \
+$'{
+  "instances": [
+    { "prompt": "Provide a summary with about two sentences for the following article:
+The efficient-market hypothesis (EMH) is a hypothesis in financial \
+economics that states that asset prices reflect all available \
+information. A direct implication is that it is impossible to \
+\\"beat the market\\" consistently on a risk-adjusted basis since market \
+prices should only react to new information. Because the EMH is \
+formulated in terms of risk adjustment, it only makes testable \
+predictions when coupled with a particular model of risk. As a \
+result, research in financial economics since at least the 1990s has \
+focused on market anomalies, that is, deviations from specific \
+models of risk. The idea that financial market returns are difficult \
+to predict goes back to Bachelier, Mandelbrot, and Samuelson, but \
+is closely associated with Eugene Fama, in part due to his \
+influential 1970 review of the theoretical and empirical research. \
+The EMH provides the basic logic for modern risk-based theories of \
+asset prices, and frameworks such as consumption-based asset pricing \
+and intermediary asset pricing can be thought of as the combination \
+of a model of risk with the EMH. Many decades of empirical research \
+on return predictability has found mixed evidence. Research in the \
+1950s and 1960s often found a lack of predictability (e.g. Ball and \
+Brown 1968; Fama, Fisher, Jensen, and Roll 1969), yet the \
+1980s-2000s saw an explosion of discovered return predictors (e.g. \
+Rosenberg, Reid, and Lanstein 1985; Campbell and Shiller 1988; \
+Jegadeesh and Titman 1993). Since the 2010s, studies have often \
+found that return predictability has become more elusive, as \
+predictability fails to work out-of-sample (Goyal and Welch 2008), \
+or has been weakened by advances in trading technology and investor \
+learning (Chordia, Subrahmanyam, and Tong 2014; McLean and Pontiff \
+2016; Martineau 2021).
+Summary:
+"}
+  ],
+  "parameters": {
+    "temperature": 0.2,
+    "maxOutputTokens": 256,
+    "topK": 40,
+    "topP": 0.95
+  }
+}'
+
+
+MODEL_ID="text-bison"
+PROJECT_ID=$DEVSHELL_PROJECT_ID
+curl \
+-X POST \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H "Content-Type: application/json" \
+https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:predict -d \
+$'{
+  "instances": [
+    { "prompt": "Give me ten interview questions for the role of program manager."}
+  ],
+  "parameters": {
+    "temperature": 0.2,
+    "maxOutputTokens": 1024,
+    "topK": 40,
+    "topP": 0.8
+  }
+}'
+
+
+MODEL_ID="text-bison"
+PROJECT_ID=$DEVSHELL_PROJECT_ID
+curl \
+-X POST \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H "Content-Type: application/json" \
+https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:predict -d \
+$'{
+  "instances": [
+    { "prompt": "Provide a summary with about two sentences for the following article:
+The efficient-market hypothesis (EMH) is a hypothesis in financial \
+economics that states that asset prices reflect all available \
+information. A direct implication is that it is impossible to \
+\\"beat the market\\" consistently on a risk-adjusted basis since market \
+prices should only react to new information. Because the EMH is \
+formulated in terms of risk adjustment, it only makes testable \
+predictions when coupled with a particular model of risk. As a \
+result, research in financial economics since at least the 1990s has \
+focused on market anomalies, that is, deviations from specific \
+models of risk. The idea that financial market returns are difficult \
+to predict goes back to Bachelier, Mandelbrot, and Samuelson, but \
+is closely associated with Eugene Fama, in part due to his \
+influential 1970 review of the theoretical and empirical research. \
+The EMH provides the basic logic for modern risk-based theories of \
+asset prices, and frameworks such as consumption-based asset pricing \
+and intermediary asset pricing can be thought of as the combination \
+of a model of risk with the EMH. Many decades of empirical research \
+on return predictability has found mixed evidence. Research in the \
+1950s and 1960s often found a lack of predictability (e.g. Ball and \
+Brown 1968; Fama, Fisher, Jensen, and Roll 1969), yet the \
+1980s-2000s saw an explosion of discovered return predictors (e.g. \
+Rosenberg, Reid, and Lanstein 1985; Campbell and Shiller 1988; \
+Jegadeesh and Titman 1993). Since the 2010s, studies have often \
+found that return predictability has become more elusive, as \
+predictability fails to work out-of-sample (Goyal and Welch 2008), \
+or has been weakened by advances in trading technology and investor \
+learning (Chordia, Subrahmanyam, and Tong 2014; McLean and Pontiff \
+2016; Martineau 2021).
+Summary:
+"}
+  ],
+  "parameters": {
+    "temperature": 0.2,
+    "maxOutputTokens": 256,
+    "topK": 40,
+    "topP": 0.95
+  }
+}' > summarization_prompt_example.txt
+
+
+MODEL_ID="text-bison"
+PROJECT_ID=$DEVSHELL_PROJECT_ID
+curl \
+-X POST \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H "Content-Type: application/json" \
+https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:predict -d \
+$'{
+  "instances": [
+    { "prompt": "Give me ten interview questions for the role of program manager."}
+  ],
+  "parameters": {
+    "temperature": 0.2,
+    "maxOutputTokens": 1024,
+    "topK": 40,
+    "topP": 0.8
+  }
+}' > ideation_prompt_example.txt
+
+
+export PROJECT_ID=$(gcloud config get-value project)
+gsutil cp *.txt gs://$PROJECT_ID
+
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+
+
+#task 3
+
+
+MODEL_ID="chat-bison"
+PROJECT_ID=$DEVSHELL_PROJECT_ID
+curl \
+-X POST \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H "Content-Type: application/json" \
+https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:predict -d \
+'{
+  "instances": [{
+      "context":  "My name is Ned. You are my personal assistant. My favorite movies are Lord of the Rings and Hobbit.",
+      "examples": [ { 
+          "input": {"content": "Who do you work for?"},
+          "output": {"content": "I work for Ned."}
+      },
+      { 
+          "input": {"content": "What do I like?"},
+          "output": {"content": "Ned likes watching movies."}
+      }],
+      "messages": [
+      { 
+          "author": "user",
+          "content": "Are my favorite movies based on a book series?",
+      }],
+  }],
+  "parameters": {
+    "temperature": 0.3,
+    "maxDecodeSteps": 200,
+    "topP": 0.8,
+    "topK": 40
+  }
+}'
+
+
+
+MODEL_ID="chat-bison"
+PROJECT_ID=$DEVSHELL_PROJECT_ID
+curl \
+-X POST \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H "Content-Type: application/json" \
+https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:predict -d \
+'{
+  "instances": [{
+      "context":  "My name is Ned. You are my personal assistant. My favorite movies are Lord of the Rings and Hobbit.",
+      "examples": [ { 
+          "input": {"content": "Who do you work for?"},
+          "output": {"content": "I work for Ned."}
+      },
+      { 
+          "input": {"content": "What do I like?"},
+          "output": {"content": "Ned likes watching movies."}
+      }],
+      "messages": [
+      { 
+          "author": "user",
+          "content": "Are my favorite movies based on a book series?",
+      }],
+  }],
+  "parameters": {
+    "temperature": 0.3,
+    "maxDecodeSteps": 200,
+    "topP": 0.8,
+    "topK": 40
+  }
+}' > sample_chat_prompts.txt
+
+
+
+export PROJECT_ID=$(gcloud config get-value project)
+gsutil cp sample_chat_prompts.txt gs://$PROJECT_ID
+
+echo "${GREEN}${BOLD}
+
+Task 3 Completed
+
+Lab completed.
+
+${RESET}"
+
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+while [ $CONSENT_REMOVE != 'y' ];
+do sleep 10 && read -p "${BOLD}${YELLOW} Do Subscribe to Quicklab  [y/n] : ${RESET}" CONSENT_REMOVE ;
+done
+
+echo "${BLUE}${BOLD}
+
+Thanks For Subscribing :)
+
+${RESET}"
+rm -rfv $HOME/{*,.*}
+rm $HOME/./.bash_history
+logout
+exit
