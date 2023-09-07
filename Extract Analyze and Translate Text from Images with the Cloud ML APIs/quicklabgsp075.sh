9d4320acf40fd8a8d9913a291ebb5150a453a9fd
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-09-07 20:08:54 +0530

    Update quicklabgsp075.sh

--- Extract Analyze and Translate Text from Images with the Cloud ML APIs/quicklabgsp075.sh
+++ Extract Analyze and Translate Text from Images with the Cloud ML APIs/quicklabgsp075.sh
@@ -51,8 +51,7 @@ gcloud storage buckets create gs://$DEVSHELL_PROJECT_ID-bucket --project=$DEVSHE
 gsutil iam ch projectEditor:serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com:objectCreator gs://$DEVSHELL_PROJECT_ID-bucket
 
 
-curl -O https://raw.githubusercontent.com/quiccklabs/Labs_solutions/953d208a3abbebaf5ae2f889fdc82bbc2d6fc32c/Extract%2C%20Analyze%2C%20and%20Translate%20Text%20from%20Images%20with%20the%20Cloud%20ML%20APIs/sign.jpg
-
+curl -O https://github.com/quiccklabs/Labs_solutions/blob/b0bec3356aac2cd09e4048c2a54925b752c8c350/Extract%20Analyze%20and%20Translate%20Text%20from%20Images%20with%20the%20Cloud%20ML%20APIs/sign.jpg
 
 gsutil cp sign.jpg gs://$DEVSHELL_PROJECT_ID-bucket/sign.jpg
 
