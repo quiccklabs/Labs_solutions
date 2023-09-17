79a15ca1c1a76cfcf35550f74e53a7e26b17ad6a
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-09-17 19:20:25 +0530

    Add files via upload

---
+++ Building Transformations and Preparing Data with Wrangler in Cloud Data Fusion/quicklab.sh
@@ -0,0 +1,81 @@
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+#export ZONE=us-central1-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
+
+export PROJECT_NUMBER=$(gcloud projects describe $GOOGLE_CLOUD_PROJECT --format="value(projectNumber)")
+echo $PROJECT_NUMBER
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com --role=roles/datafusion.serviceAgent
+
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-datafusion.iam.gserviceaccount.com --role=roles/iam.serviceAccountUser
+
+echo "${GREEN}${BOLD}
+
+Task 1 Completed
+
+${RESET}"
+
+
+
+#task 2
+
+export BUCKET=$GOOGLE_CLOUD_PROJECT
+gcloud storage buckets create gs://$BUCKET
+gcloud storage cp gs://cloud-training/OCBL163/titanic.csv gs://$BUCKET
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+
+#task 6
+
+bq mk demo_cdf
+
+echo "${GREEN}${BOLD}
+
+Task 6 Completed
+
+${RESET}"
