3d01a243696957728722e81fd4237087111cc3d8
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-09-26 00:18:42 +0530

    Update quicklabgsp151.sh

--- Cloud SQL for MySQL Qwik Start/quicklabgsp151.sh
+++ Cloud SQL for MySQL Qwik Start/quicklabgsp151.sh
@@ -4,6 +4,6 @@ gcloud sql instances create myinstance \
   --root-password=quicklab \
   --database-version=MYSQL_8_0 \
   --tier=db-n1-standard-4 \
-  --region="${ZONE%*-}"
+  --region="${ZONE%-*}"
 
 gcloud sql databases create guestbook --instance=myinstance
