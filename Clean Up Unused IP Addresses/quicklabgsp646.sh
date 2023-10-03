88bf41482adafc69bef94edbb4348a1840595169
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-10-04 03:53:07 +0530

    Update quicklabgsp646.sh

--- Clean Up Unused IP Addresses/quicklabgsp646.sh
+++ Clean Up Unused IP Addresses/quicklabgsp646.sh
@@ -1,105 +1,40 @@
 
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
-
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
-
-BOLD=`tput bold`
-RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
-
-echo "${YELLOW}${BOLD}
-
-Starting Execution 
-
-
-${RESET}"
-#gcloud auth list
-#gcloud config list project
-export PROJECT_ID=$(gcloud info --format='value(config.project)')
-#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
-#export EMAIL=$(gcloud config get-value core/account)
-#gcloud config set compute/region $region
-#gcloud config set compute/zone $region-a
-#export ZONE=$region-a
-
-
-
-#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
-#----------------------------------------------------code--------------------------------------------------# 
 
 
 gcloud services enable cloudscheduler.googleapis.com
 
 sleep 15
 
-echo "${GREEN}${BOLD}
-
-Task 1 Completed
-
-${RESET}"
-
-
 git clone https://github.com/GoogleCloudPlatform/gcf-automated-resource-cleanup.git && cd gcf-automated-resource-cleanup/
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
+export region=$REGION
+WORKDIR=$(pwd)
 
 cd $WORKDIR/unused-ip
 
 export USED_IP=used-ip-address
 export UNUSED_IP=unused-ip-address
 
-gcloud compute addresses create $USED_IP --project=$PROJECT_ID --region=$region
-gcloud compute addresses create $UNUSED_IP --project=$PROJECT_ID --region=$region
-
-gcloud compute addresses list --filter="region:($region)"
-
-echo "${GREEN}${BOLD}
-
-Task 2 Completed
-
-${RESET}"
+gcloud compute addresses create $USED_IP --project=$PROJECT_ID --region=$REGION
+gcloud compute addresses create $UNUSED_IP --project=$PROJECT_ID --region=$REGION
 
 
-export USED_IP_ADDRESS=$(gcloud compute addresses describe $USED_IP --region=$region --format=json | jq -r '.address')
+gcloud compute addresses list --filter="region:($REGION)"
 
+export USED_IP_ADDRESS=$(gcloud compute addresses describe $USED_IP --region=$REGION --format=json | jq -r '.address')
 
 gcloud compute instances create static-ip-instance \
---zone=$region-a \
+--zone=$REGION-a \
 --machine-type=n1-standard-1 \
 --subnet=default \
 --address=$USED_IP_ADDRESS
 
-echo "${GREEN}${BOLD}
-
-Task 3 Completed
-
-${RESET}"
-
-
-gcloud compute addresses list --filter="region:($region)"
+gcloud compute addresses list --filter="region:($REGION)"
 
 cat $WORKDIR/unused-ip/function.js | grep "const compute" -A 31
 
-gcloud functions deploy unused_ip_function --trigger-http --runtime=nodejs12 --region=$region --quiet
-
-echo "${GREEN}${BOLD}
-
-Task 5 Completed
-
-${RESET}"
-
+gcloud functions deploy unused_ip_function --trigger-http --runtime=nodejs12 --region=$REGION --quiet
 
 export FUNCTION_URL=$(gcloud functions describe unused_ip_function --format=json | jq -r '.httpsTrigger.url')
 
@@ -108,20 +43,11 @@ gcloud app create --region us-central
 gcloud scheduler jobs create http unused-ip-job \
 --schedule="* 2 * * *" \
 --uri=$FUNCTION_URL \
---location=$region
+--location=$REGION
+
 
 gcloud scheduler jobs run unused-ip-job \
---location=$region
-
-gcloud compute addresses list --filter="region:($region)"
+--location=$REGION
 
 
-
-echo "${GREEN}${BOLD}
-
-Task 6 Completed
-
-Lab Completed !!!
-
-${RESET}"
-
+gcloud compute addresses list --filter="region:($REGION)"
