7f6d0ea991324cd9e00f9f9e0a46bdfe7ab07b1c
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-10-04 08:35:06 +0530

    Add files via upload

---
+++ BigQuery Qwik Start  Command Line/quicklabgsp071.sh
@@ -0,0 +1,36 @@
+
+
+bq show bigquery-public-data:samples.shakespeare
+
+bq query --use_legacy_sql=false \
+'SELECT
+   word,
+   SUM(word_count) AS count
+ FROM
+   `bigquery-public-data`.samples.shakespeare
+ WHERE
+   word LIKE "%raisin%"
+ GROUP BY
+   word'
+
+bq query --use_legacy_sql=false \
+'SELECT
+   word
+ FROM
+   `bigquery-public-data`.samples.shakespeare
+ WHERE
+   word = "huzzah"'
+
+bq mk babynames
+
+curl -LO http://www.ssa.gov/OACT/babynames/names.zip
+
+unzip names.zip
+
+bq load babynames.names2010 yob2010.txt name:string,gender:string,count:integer
+
+bq query "SELECT name,count FROM babynames.names2010 WHERE gender = 'F' ORDER BY count DESC LIMIT 5"
+
+bq query "SELECT name,count FROM babynames.names2010 WHERE gender = 'M' ORDER BY count ASC LIMIT 5"
+
+bq rm -r babynames
