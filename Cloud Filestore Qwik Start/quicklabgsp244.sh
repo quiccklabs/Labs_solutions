32a80766b4862b53c3df0a5529407bb940eb538f
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-11-11 03:24:26 +0530

    Add files via upload

---
+++ Cloud Filestore Qwik Start/quicklabgsp244.sh
@@ -0,0 +1,11 @@
+
+
+
+
+gcloud compute instances create nfs-client --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=nfs-client,image=projects/debian-cloud/global/images/debian-11-bullseye-v20231010,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+
+
+gcloud services enable  file.googleapis.com
+
+  
+gcloud filestore instances create nfs-server --zone=$ZONE --tier=BASIC_HDD --file-share=name="vol1",capacity=1TB --network=name="default" 
