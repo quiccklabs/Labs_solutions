dbfa190439f0ac9ce0f283c0df5a325b1bb7ac3c
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-11-15 01:12:13 +0530

    Update quicklab.sh

--- Configuring and Using Cloud Logging and Cloud Monitoring/quicklab.sh
+++ Configuring and Using Cloud Logging and Cloud Monitoring/quicklab.sh
@@ -22,6 +22,28 @@ sudo chmod +x pubsub.sh
 
 
 bq mk project_logs
+slepp 15
+export PROJECT_NUMBER=$(gcloud projects describe $DEVSHELL_PROJECT_ID --format="json(projectNumber)" --quiet | jq -r '.projectNumber')
+
+# Export the service account to a variable
+SERVICE_ACCOUNT=service-$PROJECT_NUMBER@gcp-sa-logging.iam.gserviceaccount.com
+
+# Grant the roles/bigquery.dataEditor role to the service account
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=serviceAccount:$SERVICE_ACCOUNT \
+  --role=roles/bigquery.dataEditor
+
+# Export the table ID to a variable
+TABLE_ID=$(bq ls --project_id $DEVSHELL_PROJECT_ID --dataset_id project_logs --format=json | jq -r '.[0].tableReference.tableId')
+
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+  logName, resource.type, resource.labels.zone, resource.labels.project_id,
+FROM
+  \`$DEVSHELL_PROJECT_ID.project_logs.$TABLE_ID\`
+"
 
 gcloud alpha logging sinks create vm_logs bigquery.googleapis.com/projects/$DEVSHELL_PROJECT_ID/datasets/project_logs --log-filter='resource.type="gce_instance"'
 
@@ -29,6 +51,8 @@ gcloud alpha logging sinks create load_bal_logs bigquery.googleapis.com/projects
 
 
 gcloud logging read "resource.type=gce_instance AND logName=projects/$DEVSHELL_PROJECT_ID/logs/syslog AND textPayload:SyncAddress" --limit 10 --format json
+
+sleep 15
 
 export PROJECT_NUMBER=$(gcloud projects describe $DEVSHELL_PROJECT_ID --format="json(projectNumber)" --quiet | jq -r '.projectNumber')
 
@@ -57,3 +81,27 @@ gcloud logging metrics create 403s \
     --project=$DEVSHELL_PROJECT_ID \
     --description="Subscribe to quicklab" \
     --log-filter='resource.type="gce_instance" AND log_name="projects/'$DEVSHELL_PROJECT_ID'/logs/syslog"'
+
+sleep 15
+
+export PROJECT_NUMBER=$(gcloud projects describe $DEVSHELL_PROJECT_ID --format="json(projectNumber)" --quiet | jq -r '.projectNumber')
+
+# Export the service account to a variable
+SERVICE_ACCOUNT=service-$PROJECT_NUMBER@gcp-sa-logging.iam.gserviceaccount.com
+
+# Grant the roles/bigquery.dataEditor role to the service account
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=serviceAccount:$SERVICE_ACCOUNT \
+  --role=roles/bigquery.dataEditor
+
+# Export the table ID to a variable
+TABLE_ID=$(bq ls --project_id $DEVSHELL_PROJECT_ID --dataset_id project_logs --format=json | jq -r '.[0].tableReference.tableId')
+
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+  logName, resource.type, resource.labels.zone, resource.labels.project_id,
+FROM
+  \`$DEVSHELL_PROJECT_ID.project_logs.$TABLE_ID\`
+"
