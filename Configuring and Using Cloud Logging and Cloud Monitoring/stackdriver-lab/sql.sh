000d23603456d65884b7eb2081adc61682211f56
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-11-14 20:38:42 +0530

    Delete Configuring and Using Cloud Logging and Cloud Monitoring/stackdriver-lab directory

--- Configuring and Using Cloud Logging and Cloud Monitoring/stackdriver-lab/sql.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcloud services enable sql-component.googleapis.com
-gcloud services enable sqladmin.googleapis.com
-gcloud sql instances create demo-sql
-gcloud sql databases create demo-db --instance demo-sql
-gcloud sql users set-password root --instance demo-sql --password demo-pass
