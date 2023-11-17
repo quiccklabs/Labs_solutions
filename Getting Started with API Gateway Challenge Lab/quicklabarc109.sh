d3075df238759ba2d64181e6d0c993bb36a9899d
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-11-18 05:26:08 +0530

    Update quicklabarc109.sh

--- Getting Started with API Gateway Challenge Lab/quicklabarc109.sh
+++ Getting Started with API Gateway Challenge Lab/quicklabarc109.sh
@@ -2,8 +2,11 @@
 
 
 
-
 gcloud auth list
+
+gcloud services enable apigateway.googleapis.com
+
+sleep 10
 
 mkdir quicklab
 cd quicklab
@@ -38,9 +41,26 @@ sleep 40
 
 export PROJECT_NUMBER=$(gcloud projects describe $DEVSHELL_PROJECT_ID --format="json(projectNumber)" --quiet | jq -r '.projectNumber')
 
-gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
-  --member=serviceAccount:service-$PROJECT_NUMBER@gcf-admin-robot.iam.gserviceaccount.com \
-  --role=roles/artifactregistry.reader
+# Set the service account email
+SERVICE_ACCOUNT="service-$PROJECT_NUMBER@gcf-admin-robot.iam.gserviceaccount.com"
+
+# Get the current IAM policy
+IAM_POLICY=$(gcloud projects get-iam-policy $DEVSHELL_PROJECT_ID --format=json)
+
+# Check if the binding exists
+if [[ "$IAM_POLICY" == *"$SERVICE_ACCOUNT"* && "$IAM_POLICY" == *"roles/artifactregistry.reader"* ]]; then
+  echo "IAM binding exists for service account: $SERVICE_ACCOUNT with role roles/artifactregistry.reader"
+else
+  echo "IAM binding does not exist for service account: $SERVICE_ACCOUNT with role roles/artifactregistry.reader"
+  
+  # Create the IAM binding
+  gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member=serviceAccount:$SERVICE_ACCOUNT \
+    --role=roles/artifactregistry.reader
+
+  echo "IAM binding created for service account: $SERVICE_ACCOUNT with role roles/artifactregistry.reader"
+fi
+
 
 
 gcloud functions deploy GCFunction \
@@ -55,9 +75,7 @@ gcloud functions deploy GCFunction \
 
 
 
-gcloud services enable apigateway.googleapis.com
 
-sleep 10
 
 gcloud pubsub topics create demo-topic
 
@@ -151,7 +169,36 @@ gcloud api-gateway api-configs create gcfunction-api \
 
 
 
+# Set the service account email
+SERVICE_ACCOUNT="service-$PROJECT_NUMBER@gcf-admin-robot.iam.gserviceaccount.com"
 
+# Get the current IAM policy
+IAM_POLICY=$(gcloud projects get-iam-policy $DEVSHELL_PROJECT_ID --format=json)
+
+# Check if the binding exists
+if [[ "$IAM_POLICY" == *"$SERVICE_ACCOUNT"* && "$IAM_POLICY" == *"roles/artifactregistry.reader"* ]]; then
+  echo "IAM binding exists for service account: $SERVICE_ACCOUNT with role roles/artifactregistry.reader"
+else
+  echo "IAM binding does not exist for service account: $SERVICE_ACCOUNT with role roles/artifactregistry.reader"
+  
+  # Create the IAM binding
+  gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member=serviceAccount:$SERVICE_ACCOUNT \
+    --role=roles/artifactregistry.reader
+
+  echo "IAM binding created for service account: $SERVICE_ACCOUNT with role roles/artifactregistry.reader"
+fi
+
+
+gcloud functions deploy GCFunction \
+  --runtime=nodejs20 \
+  --trigger-http \
+  --gen2 \
+  --allow-unauthenticated \
+  --entry-point=helloWorld \
+  --region=$REGION \
+  --max-instances 5 \
+  --source=./
 
 
 
