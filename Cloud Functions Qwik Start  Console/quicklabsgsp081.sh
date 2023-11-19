a756fba01715dd1c41f6aa7011d68b219fa6dbc4
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-11-19 11:46:43 +0530

    Update quicklabsgsp081.sh

--- Cloud Functions Qwik Start  Console/quicklabsgsp081.sh
+++ Cloud Functions Qwik Start  Console/quicklabsgsp081.sh
@@ -69,4 +69,41 @@ gcloud functions deploy GCFunction \
 DATA=$(printf 'Subscribe to quicklab' | base64) && gcloud functions call GCFunction --region=$REGION --data '{"data":"'$DATA'"}'
 
 
+sleep 50
+
+sleep 30
+
+export PROJECT_NUMBER=$(gcloud projects describe $DEVSHELL_PROJECT_ID --format="json(projectNumber)" --quiet | jq -r '.projectNumber')
+
+# Set the service account email
+SERVICE_ACCOUNT="service-$PROJECT_NUMBER@gcf-admin-robot.iam.gserviceaccount.com"
+
+# Get the current IAM policy
+IAM_POLICY=$(gcloud projects get-iam-policy $DEVSHELL_PROJECT_ID --format=json)
+
+# Check if the binding exists
+if [[ "$IAM_POLICY" == *"$SERVICE_ACCOUNT"* && "$IAM_POLICY" == *"roles/artifactregistry.reader"* ]]; then
+  echo "IAM binding exists for service account: $SERVICE_ACCOUNT with role roles/artifactregistry.reader"
+else
+  echo "IAM binding does not exist for service account: $SERVICE_ACCOUNT with role roles/artifactregistry.reader"
+  
+  # Create the IAM binding
+  gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member=serviceAccount:$SERVICE_ACCOUNT \
+    --role=roles/artifactregistry.reader
+
+  echo "IAM binding created for service account: $SERVICE_ACCOUNT with role roles/artifactregistry.reader"
+fi
+
+
+gcloud functions deploy GCFunction \
+  --region=$REGION \
+  --gen2 \
+  --trigger-http \
+  --runtime=nodejs20 \
+  --allow-unauthenticated \
+  --max-instances=5
+
+
+DATA=$(printf 'Subscribe to quicklab' | base64) && gcloud functions call GCFunction --region=$REGION --data '{"data":"'$DATA'"}'
 
