f495733801f8f532cb53a5242db083cf04a8138f
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-12-04 00:22:04 +0530

    Update quicklabgsp122.sh

--- Google Cloud SDK Qwik Start  Redha tCentos/quicklabgsp122.sh
+++ Google Cloud SDK Qwik Start  Redha tCentos/quicklabgsp122.sh
@@ -4,6 +4,8 @@
 
 gcloud compute instances create quicklab --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=quicklab,image=projects/centos-cloud/global/images/centos-7-v20231010,mode=rw,size=20,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
 
+sleep 30
+
 gcloud compute ssh quicklab --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="\
 sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
 [google-cloud-sdk]
