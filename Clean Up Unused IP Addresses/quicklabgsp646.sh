059774f17509ca3c0dd760d5201aca3ca719a79a
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-12-06 02:26:58 +0530

    Add files via upload

--- Clean Up Unused IP Addresses/quicklabgsp646.sh
+++ Clean Up Unused IP Addresses/quicklabgsp646.sh
@@ -1,53 +1,56 @@
 
 
 
+
+
+
 gcloud services enable cloudscheduler.googleapis.com
-
-sleep 15
-
+sleep 20
 git clone https://github.com/GoogleCloudPlatform/gcf-automated-resource-cleanup.git && cd gcf-automated-resource-cleanup/
-
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
-export region=$REGION
+export REGION="${ZONE%-*}"
 WORKDIR=$(pwd)
-
 cd $WORKDIR/unused-ip
-
 export USED_IP=used-ip-address
 export UNUSED_IP=unused-ip-address
-
 gcloud compute addresses create $USED_IP --project=$PROJECT_ID --region=$REGION
+sleep 10
 gcloud compute addresses create $UNUSED_IP --project=$PROJECT_ID --region=$REGION
-
+sleep 20
 
 gcloud compute addresses list --filter="region:($REGION)"
-
 export USED_IP_ADDRESS=$(gcloud compute addresses describe $USED_IP --region=$REGION --format=json | jq -r '.address')
 
 gcloud compute instances create static-ip-instance \
---zone=$REGION-a \
---machine-type=n1-standard-1 \
+--zone=$ZONE \
+--machine-type=e2-medium \
 --subnet=default \
 --address=$USED_IP_ADDRESS
-
-gcloud compute addresses list --filter="region:($REGION)"
-
-cat $WORKDIR/unused-ip/function.js | grep "const compute" -A 31
+sleep 20
 
 gcloud functions deploy unused_ip_function --trigger-http --runtime=nodejs12 --region=$REGION --quiet
 
-export FUNCTION_URL=$(gcloud functions describe unused_ip_function --format=json | jq -r '.httpsTrigger.url')
+export FUNCTION_URL=$(gcloud functions describe unused_ip_function --region=$REGION --format=json | jq -r '.httpsTrigger.url')
 
-gcloud app create --region us-central
+if [ "$REGION" == "us-central1" ]; then
+  gcloud app create --region us-central
+else
+  gcloud app create --region "$REGION"
+fi
 
 gcloud scheduler jobs create http unused-ip-job \
 --schedule="* 2 * * *" \
 --uri=$FUNCTION_URL \
---location=$REGION
-
+--location=$REGION \
+--oidc-service-account-email=$DEVSHELL_PROJECT_ID@appspot.gserviceaccount.com
 
 gcloud scheduler jobs run unused-ip-job \
 --location=$REGION
-
-
 gcloud compute addresses list --filter="region:($REGION)"
+ 
+
+
+
+
+
+
