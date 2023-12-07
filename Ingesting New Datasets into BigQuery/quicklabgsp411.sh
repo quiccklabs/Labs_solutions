99b94b53f3b1a8214784a440e1d0bd446da4ff8e
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-12-07 18:37:07 +0530

    Update quicklabgsp411.sh

--- Ingesting New Datasets into BigQuery/quicklabgsp411.sh
+++ Ingesting New Datasets into BigQuery/quicklabgsp411.sh
@@ -17,3 +17,36 @@ bq --location=US load --source_format=CSV --autodetect --skip_leading_rows=1 eco
 
 bq --location=US load --source_format=CSV --autodetect --skip_leading_rows=1 ecommerce.products gs://data-insights-course/exports/products.csv
 
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+  *,
+  SAFE_DIVIDE(orderedQuantity,stockLevel) AS ratio
+FROM
+  ecommerce.products
+WHERE
+# include products that have been ordered and
+# are 80% through their inventory
+orderedQuantity > 0
+AND SAFE_DIVIDE(orderedQuantity,stockLevel) >= .8
+ORDER BY
+  restockingLeadTime DESC
+"
+
+cat > external_table_definition.json <<EOF
+{
+  "sourceFormat": "GOOGLE_SHEETS",
+  "sourceUris": ["https://docs.google.com/spreadsheets/d/1Pyr2ifVgC82eCDNxBKgEXc33fkzMTPa2/edit?usp=sharing"],
+  "schema": {
+    "fields": [
+      {"name": "column1", "type": "STRING"},
+      {"name": "column2", "type": "INTEGER"},
+      {"name": "column3", "type": "FLOAT"}
+    ]
+  }
+}
+EOF
+
+bq mk --external_table_definition=external_table_definition.json ecommerce.products_comments
