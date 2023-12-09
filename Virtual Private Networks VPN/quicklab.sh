9a5e6158d42ba9f27d14727b7f5e206c7e6440d7
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2023-12-09 18:22:16 +0530

    Update quicklab.sh

--- Virtual Private Networks VPN/quicklab.sh
+++ Virtual Private Networks VPN/quicklab.sh
@@ -1,6 +1,50 @@
 
 
 
+
+export REGION_1="${ZONE_1%-*}"
+
+export REGION_2="${ZONE_2%-*}"
+
+
+
+
+gcloud compute networks create vpn-network-1 --subnet-mode custom
+
+gcloud compute networks subnets create subnet-a \
+--network vpn-network-1 --range 10.1.1.0/24 --region "$REGION_1"
+
+gcloud compute firewall-rules create network-1-allow-custom \
+  --network vpn-network-1 \
+  --allow tcp:0-65535,udp:0-65535,icmp \
+  --source-ranges 10.0.0.0/8
+
+gcloud compute firewall-rules create network-1-allow-ssh-icmp \
+    --network vpn-network-1 \
+    --allow tcp:22,icmp
+
+gcloud compute instances create server-1 --machine-type=e2-medium --zone $ZONE_1 --subnet subnet-a
+
+gcloud compute networks create vpn-network-2 --subnet-mode custom
+
+gcloud compute networks subnets create subnet-b \
+--network vpn-network-2 --range 192.168.1.0/24 --region $REGION_2
+
+gcloud compute firewall-rules create network-2-allow-custom \
+  --network vpn-network-2 \
+  --allow tcp:0-65535,udp:0-65535,icmp \
+  --source-ranges 192.168.0.0/16
+
+gcloud compute firewall-rules create network-2-allow-ssh-icmp \
+    --network vpn-network-2 \
+    --allow tcp:22,icmp
+
+gcloud compute instances create server-2 --machine-type=e2-medium --zone $ZONE_2 --subnet subnet-b
+
+
+
+## OLD
+
 gcloud compute addresses create vpn-1-static-ip --project=$DEVSHELL_PROJECT_ID --region=$REGION_1
 
 gcloud compute addresses create vpn-2-static-ip --project=$DEVSHELL_PROJECT_ID --region=$REGION_2
