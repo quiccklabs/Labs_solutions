e05bb7d0f9b26e060c8cad092e474086761be5f9
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-01-04 20:33:36 +0530

    Add files via upload

---
+++ Provision Cloud Infrastructure with Duet AI/quicklab.sh
@@ -0,0 +1,23 @@
+
+PROJECT_ID=$(gcloud config get-value project)
+echo "PROJECT_ID=${PROJECT_ID}"
+echo "REGION=${REGION}"
+
+
+USER=$(gcloud config get-value account 2> /dev/null)
+echo "USER=${USER}"
+
+gcloud services enable cloudaicompanion.googleapis.com --project ${PROJECT_ID}
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/cloudaicompanion.user
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/serviceusage.serviceUsageViewer
+
+sleep 20
+
+gcloud container clusters create-auto duet-ai-demo --region $REGION
+
+kubectl create deployment hello-server --image=us-docker.pkg.dev/google-samples/containers/gke/hello-app:1.0
+
+kubectl expose deployment hello-server --type LoadBalancer --port 80 --target-port 8080 
+
+kubectl get service hello-server
