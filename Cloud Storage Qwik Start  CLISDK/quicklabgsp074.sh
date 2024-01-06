e7d39c51e60a5e2f1e4072f4d3aae4fcb32a877d
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-01-06 11:00:57 +0530

    Add files via upload

---
+++ Cloud Storage Qwik Start  CLISDK/quicklabgsp074.sh
@@ -0,0 +1,18 @@
+
+
+
+
+gcloud config set compute/region $REGION
+
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID
+
+curl https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/800px-Ada_Lovelace_portrait.jpg --output ada.jpg
+
+gsutil cp ada.jpg gs://$DEVSHELL_PROJECT_ID
+
+gsutil cp -r gs://$DEVSHELL_PROJECT_ID/ada.jpg .
+
+gsutil cp gs://$DEVSHELL_PROJECT_ID/ada.jpg gs://$DEVSHELL_PROJECT_ID/image-folder/
+
+gsutil acl ch -u AllUsers:R gs://$DEVSHELL_PROJECT_ID/ada.jpg
