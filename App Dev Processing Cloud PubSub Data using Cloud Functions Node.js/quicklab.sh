6374c8779ee7df3bda10815671e02f997e7c64d3
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-01-24 20:39:25 +0530

    Update quicklab.sh

--- App Dev Processing Cloud PubSub Data using Cloud Functions Node.js/quicklab.sh
+++ App Dev Processing Cloud PubSub Data using Cloud Functions Node.js/quicklab.sh
@@ -1,4 +1,5 @@
 
+
 gcloud auth list
 
 gcloud services enable cloudfunctions.googleapis.com
@@ -14,6 +15,45 @@ cd ~/cloudfunctions/start
 . prepare_environment.sh
 
 
+mkdir quicklab && cd quicklab
+
+
+
+cat > index.js <<EOF_END
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.helloWorld = (req, res) => {
+    let message = req.query.message || req.body.message || 'Hello World!';
+    res.status(200).send(message);
+  };
+  
+EOF_END
+
+cat > package.json <<EOF_END
+{
+    "name": "sample-http",
+    "version": "0.0.1"
+  }
+  
+EOF_END
+
+
+
+  gcloud functions deploy process-feedback \
+    --runtime nodejs20 \
+    --entry-point helloWorld \
+    --source . \
+    --trigger-topic feedback \
+    --max-instances 1 \
+    --no-allow-unauthenticated \
+    --quiet 
+
+cd ..
+
 cd function
 
 rm index.js
@@ -24,3 +64,13 @@ zip cf.zip *.js*
 
 gcloud storage cp cf.zip gs://$GCLOUD_BUCKET/
 
+
+
+gcloud functions deploy process-feedback \
+    --runtime nodejs20 \
+    --entry-point subscribe \
+    --trigger-topic feedback \
+    --max-instances 1 \
+    --no-allow-unauthenticated \
+    --source gs://$GCLOUD_BUCKET/cf.zip \
+    --allow-unauthenticated
