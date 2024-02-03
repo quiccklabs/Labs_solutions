1f591128fb7177c5b55d2a2afe0a1875eefb17d5
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-02-03 16:45:00 +0530

    Update task1.sh

--- Cloud Storage/task1.sh
+++ Cloud Storage/task1.sh
@@ -135,7 +135,7 @@ cp setup.html firstlevel/secondlevel
 gsutil rsync -r ./firstlevel gs://$BUCKET_NAME_1/firstlevel
 
 
-
+sleep 30
 
 gcloud compute instances create crossproject --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --create-disk=auto-delete=yes,boot=yes,device-name=crossproject,image=projects/debian-cloud/global/images/debian-11-bullseye-v20240110,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
 
