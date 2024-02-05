9834b40d8d1367a8a9308b117b883e22dc0c2a56
Billy G <10028885+GibzB@users.noreply.github.com>
2024-02-05 15:04:01 +0300

    Update task2.sh

--- Cloud Storage/task2.sh
+++ Cloud Storage/task2.sh
@@ -26,28 +26,8 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member="serviceAcc
 # Generate and download the JSON key file
 gcloud iam service-accounts keys create credentials.json --iam-account=cross-project-storage@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
 
-# Create VM instance
-gcloud compute instances create crossproject \
-  --project=$DEVSHELL_PROJECT_ID \
-  --zone=$ZONE \
-  --machine-type=e2-medium \
-  --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
-  --metadata=enable-oslogin=true \
-  --maintenance-policy=MIGRATE \
-  --provisioning-model=STANDARD \
-  --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
-  --create-disk="auto-delete=yes,boot=yes,device-name=crossproject,image=projects/debian-cloud/global/images/debian-11-bullseye-v20240110,mode=rw,size=10,type=pd-balanced" \
-  --no-shielded-secure-boot \
-  --shielded-vtpm \
-  --shielded-integrity-monitoring \
-  --labels=goog-ec-src=vm_add-gcloud \
-  --reservation-affinity=any
 
 
 
-# Change storage roles
-gcloud projects add-iam-policy-binding YOUR_PROJECT_ID \
-    --member serviceAccount:cross-project-storage@YOUR_PROJECT_ID.iam.gserviceaccount.com \
-    --role roles/storage.objectAdmin
 
 
