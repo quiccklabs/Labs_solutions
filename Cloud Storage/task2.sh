745914126844b930df9ae3bcf54c5d9bc646e3a1
Billy G <10028885+GibzB@users.noreply.github.com>
2024-02-05 14:28:42 +0300

    Update task2.sh

--- Cloud Storage/task2.sh
+++ Cloud Storage/task2.sh
@@ -35,3 +35,24 @@ gcloud compute instances create crossproject \
     --boot-disk-size 10GB \
     --boot-disk-type pd-balanced \
 
+# ssh to vm
+gcloud compute ssh crossproject
+
+sleep 10
+
+export BUCKET_NAME_2=test-2023011502
+export FILE_NAME=ttcp.csv
+echo $BUCKET_NAME_2/$FILE_NAME
+
+# Authorize the VM and access file
+$ ls -l
+total 4
+-rw-r--r-- 1 student-01-2f50fc858c40 google-sudoers 2381 Jan 15 10:26 credentials.json
+
+# change policy
+gcloud projects add-iam-policy-binding YOUR_PROJECT_ID \
+    --member serviceAccount:cross-project-storage@YOUR_PROJECT_ID.iam.gserviceaccount.com \
+    --role roles/storage.objectAdmin
+
+# Upload file again
+gsutil cp credentials.json gs://$BUCKET_NAME_2/
