3ea1c3a8bf56dd91da4f3137721a949d8bacc519
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-02-16 01:53:47 +0530

    Add files via upload

---
+++ App Dev Setting up a Development Environment Python/quicklab.sh
@@ -0,0 +1,27 @@
+
+
+
+
+gcloud compute instances create dev-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=dev-instance,image=projects/debian-cloud/global/images/debian-11-bullseye-v20240213,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+
+gcloud compute firewall-rules create allow-http --allow tcp:80 --description "Allow HTTP traffic" --direction INGRESS --target-tags http-server --network default
+
+
+
+cat > quicklab.sh <<'EOF_END'
+sudo apt-get update
+sudo apt-get install git -y
+sudo apt-get install python3-setuptools python3-dev build-essential -y
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
+python3 --version
+pip3 --version
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+ln -s ~/training-data-analyst/courses/developingapps/v1.3/python/devenv ~/devenv
+cd ~/devenv/
+sudo python3 server.py
+EOF_END
+
+gcloud compute scp quicklab.sh dev-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh dev-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/quicklab.sh"
