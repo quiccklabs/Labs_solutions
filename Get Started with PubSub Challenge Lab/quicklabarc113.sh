3301b25ac6bfdaec3634f75fd82e319d93cdf53e
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-02-24 22:10:02 +0530

    Update quicklabarc113.sh

--- Get Started with PubSub Challenge Lab/quicklabarc113.sh
+++ Get Started with PubSub Challenge Lab/quicklabarc113.sh
@@ -1,28 +1,26 @@
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
-
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
-
-BOLD=`tput bold`
-RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
-
-
-
 #!/bin/bash
+
+# Define color variables
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+
+BG_BLACK=$(tput setab 0)
+BG_RED=$(tput setab 1)
+BG_GREEN=$(tput setab 2)
+BG_YELLOW=$(tput setab 3)
+BG_BLUE=$(tput setab 4)
+BG_MAGENTA=$(tput setab 5)
+BG_CYAN=$(tput setab 6)
+BG_WHITE=$(tput setab 7)
+
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
 
 # Function to run form 1 code
 run_form_1() {
@@ -92,7 +90,8 @@ run_form_3() {
             --location=$REGION --topic=cloud-pubsub-topic-lite
 }
 
-echo "${BLUE}${BOLD}
+# Main script block
+echo "${BLUE}${BOLD}"
 
 # Get the form number from user input
 read -p "Enter the Form number (1, 2, or 3): " form_number
