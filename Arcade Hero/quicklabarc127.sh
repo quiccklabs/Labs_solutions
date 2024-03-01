4ec175e66c652c05749ea2157df41cd468e0ffd2
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-03-01 15:35:03 +0530

    Add files via upload

---
+++ Arcade Hero/quicklabarc127.sh
@@ -0,0 +1,14 @@
+
+
+
+gcloud compute networks create staging --subnet-mode=custom --project=$DEVSHELL_PROJECT_ID
+
+gcloud compute networks create development --project=$DEVSHELL_PROJECT_ID
+
+gcloud compute networks subnets create dev-1 \
+    --network=development \
+    --region=us-west1 \
+    --range=10.1.0.0/24 \
+    --project=$DEVSHELL_PROJECT_ID
+
+
