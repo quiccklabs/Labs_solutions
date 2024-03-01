82b6a6e4b06089ed111681c9d45232ac67e0af8f
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-03-01 23:55:18 +0530

    Update quicklabarc127.sh

--- Arcade Hero/quicklabarc127.sh
+++ Arcade Hero/quicklabarc127.sh
@@ -7,17 +7,10 @@ gcloud compute networks create development --project=$DEVSHELL_PROJECT_ID
 
 gcloud compute networks subnets create dev-1 \
     --network=development \
-    --region=us-west1 \
+    --region=$REGION \
     --range=10.1.0.0/24 \
     --project=$DEVSHELL_PROJECT_ID
 
-gcloud compute networks subnets delete dev-1 --region=us-west1 --quiet
-
-gcloud compute networks subnets create dev-1 \
-    --network=development \
-    --region=us-east1 \
-    --range=10.1.0.0/24 \
-    --project=$DEVSHELL_PROJECT_ID
 
 
 
