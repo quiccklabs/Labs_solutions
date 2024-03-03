4cb98282a9649acb66fd30f26da43d2b0117667d
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-03-03 14:15:36 +0530

    Add files via upload

---
+++ Terraform Fundamentals/quicklabgsp156.sh
@@ -0,0 +1,29 @@
+
+
+
+gcloud auth list 
+
+cat > instance.tf <<EOF_END
+resource "google_compute_instance" "terraform" {
+  project      = "$DEVSHELL_PROJECT_ID"
+  name         = "terraform"
+  machine_type = "e2-medium"
+  zone         = "$ZONE"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+EOF_END
+
+terraform init
+terraform plan
+terraform apply --auto-approve
