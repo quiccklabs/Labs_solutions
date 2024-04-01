acbe5d1e1ba7c7a66d0896cd56c80bef0cde559e
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-04-02 00:04:11 +0530

    Add files via upload

---
+++ Explore and Evaluate Models using Model Garden/quicklabgsp1166.sh
@@ -0,0 +1,21 @@
+
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' 
+
+echo -e "${GREEN}Open text-bison in Google Cloud Console:${NC}"
+echo "   https://console.cloud.google.com/vertex-ai/publishers/google/model-garden/text-bison?project=$DEVSHELL_PROJECT_ID"
+echo " "
+
+echo "-----------------------------------------------"
+echo " "
+
+echo -e "${BLUE}Open owlvit-base-patch32 in Google Cloud Console:${NC}"
+echo "   https://console.cloud.google.com/vertex-ai/publishers/google/model-garden/owlvit-base-patch32?project=$DEVSHELL_PROJECT_ID"
+echo " "
+
+echo "-----------------------------------------------"
+echo " "
+
+echo -e "${GREEN}Open bert-base in Google Cloud Console:${NC}"
+echo "   https://console.cloud.google.com/vertex-ai/publishers/google/model-garden/bert-base?project=$DEVSHELL_PROJECT_ID"
