4c218630e72e93c6c39d93d4ebe386003b44de2e
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-04-02 13:26:02 +0530

    Add files via upload

---
+++ Classify Text into Categories with the Natural Language API/quicklabgsp063.sh
@@ -0,0 +1,42 @@
+
+
+
+gcloud services enable language.googleapis.com
+
+
+cat > prepare_disk.sh <<'EOF_END'
+
+gcloud compute ssh linux-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="export API_KEY=$KEY && bash /tmp/prepare_disk.sh"
+
+export API_KEY="$API_KEY"  # Export the API key to the script
+
+cat > request.json <<EOF
+{
+    "document":{
+      "type":"PLAIN_TEXT",
+      "content":"A Smoky Lobster Salad With a Tapa Twist. This spin on the Spanish pulpo a la gallega skips the octopus, but keeps the sea salt, olive oil, pimentón and boiled potatoes."
+    }
+  }
+EOF
+
+
+curl "https://language.googleapis.com/v1/documents:classifyText?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request.json
+
+  curl "https://language.googleapis.com/v1/documents:classifyText?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request.json > result.json
+
+EOF_END
+
+ZONE="$(gcloud compute instances list --project=$DEVSHELL_PROJECT_ID --format='value(ZONE)')"
+
+
+gcloud compute scp prepare_disk.sh linux-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh linux-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="export API_KEY=$KEY && bash /tmp/prepare_disk.sh"
+
+
+
+bq --location=US mk --dataset $DEVSHELL_PROJECT_ID:news_classification_dataset
+
+bq mk --table $DEVSHELL_PROJECT_ID:news_classification_dataset.article_data article_text:STRING,category:STRING,confidence:FLOAT
