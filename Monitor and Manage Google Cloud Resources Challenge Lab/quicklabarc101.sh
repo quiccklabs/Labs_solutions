45f3aaa05f5c629ff6bf93fcbbd02fbfae015f23
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-04-04 23:24:58 +0530

    Add files via upload

---
+++ Monitor and Manage Google Cloud Resources Challenge Lab/quicklabarc101.sh
@@ -0,0 +1,171 @@
+
+
+
+
+
+gcloud config set compute/region $REGION
+
+gsutil mb -p $DEVSHELL_PROJECT_ID gs://$BUCKET_NAME
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=user:$SECOND_USER --role=roles/storage.objectViewer
+
+gcloud pubsub topics create $TOPIC_NAME
+
+
+mkdir quicklab && cd quicklab
+
+
+cat > index.js <<'EOF_END'
+/* globals exports, require */
+//jshint strict: false
+//jshint esversion: 6
+"use strict";
+const crc32 = require("fast-crc32c");
+const { Storage } = require('@google-cloud/storage');
+const gcs = new Storage();
+const { PubSub } = require('@google-cloud/pubsub');
+const imagemagick = require("imagemagick-stream");
+
+exports.thumbnail = (event, context) => {
+  const fileName = event.name;
+  const bucketName = event.bucket;
+  const size = "64x64"
+  const bucket = gcs.bucket(bucketName);
+  const topicName = "REPLACE_WITH_YOUR_TOPIC ID";
+  const pubsub = new PubSub();
+  if ( fileName.search("64x64_thumbnail") == -1 ){
+    // doesn't have a thumbnail, get the filename extension
+    var filename_split = fileName.split('.');
+    var filename_ext = filename_split[filename_split.length - 1];
+    var filename_without_ext = fileName.substring(0, fileName.length - filename_ext.length );
+    if (filename_ext.toLowerCase() == 'png' || filename_ext.toLowerCase() == 'jpg'){
+      // only support png and jpg at this point
+      console.log(`Processing Original: gs://${bucketName}/${fileName}`);
+      const gcsObject = bucket.file(fileName);
+      let newFilename = filename_without_ext + size + '_thumbnail.' + filename_ext;
+      let gcsNewObject = bucket.file(newFilename);
+      let srcStream = gcsObject.createReadStream();
+      let dstStream = gcsNewObject.createWriteStream();
+      let resize = imagemagick().resize(size).quality(90);
+      srcStream.pipe(resize).pipe(dstStream);
+      return new Promise((resolve, reject) => {
+        dstStream
+          .on("error", (err) => {
+            console.log(`Error: ${err}`);
+            reject(err);
+          })
+          .on("finish", () => {
+            console.log(`Success: ${fileName} → ${newFilename}`);
+              // set the content-type
+              gcsNewObject.setMetadata(
+              {
+                contentType: 'image/'+ filename_ext.toLowerCase()
+              }, function(err, apiResponse) {});
+              pubsub
+                .topic(topicName)
+                .publisher()
+                .publish(Buffer.from(newFilename))
+                .then(messageId => {
+                  console.log(`Message ${messageId} published.`);
+                })
+                .catch(err => {
+                  console.error('ERROR:', err);
+                });
+          });
+      });
+    }
+    else {
+      console.log(`gs://${bucketName}/${fileName} is not an image I can handle`);
+    }
+  }
+  else {
+    console.log(`gs://${bucketName}/${fileName} already has a thumbnail`);
+  }
+};
+EOF_END
+
+sed -i "16c\  const topicName = '$TOPIC_NAME';" index.js
+
+cat > package.json <<'EOF_END'
+{
+    "name": "thumbnails",
+    "version": "1.0.0",
+    "description": "Create Thumbnail of uploaded image",
+    "scripts": {
+      "start": "node index.js"
+    },
+    "dependencies": {
+      "@google-cloud/pubsub": "^2.0.0",
+      "@google-cloud/storage": "^5.0.0",
+      "fast-crc32c": "1.0.4",
+      "imagemagick-stream": "4.1.1"
+    },
+    "devDependencies": {},
+    "engines": {
+      "node": ">=4.3.2"
+    }
+  }
+EOF_END
+
+
+export PROJECT_ID=$(gcloud config get-value project)
+PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$PROJECT_ID" --format='value(project_number)')
+SERVICE_ACCOUNT=$(gsutil kms serviceaccount -p $PROJECT_NUMBER)
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member serviceAccount:$SERVICE_ACCOUNT \
+  --role roles/artifactregistry.reader
+  
+
+
+gcloud functions deploy $FUNCTION_NAME \
+--runtime=nodejs14 \
+--region=$REGION \
+--source=. \
+--entry-point=thumbnail \
+--trigger-bucket $BUCKET_NAME 
+
+
+wget https://storage.googleapis.com/cloud-training/arc101/travel.jpg
+
+gsutil cp travel.jpg gs://$BUCKET_NAME
+
+
+
+cat > app-engine-error-percent-policy.json <<EOF_END
+{
+    "displayName": "Active Cloud Function Instances",
+    "userLabels": {},
+    "conditions": [
+      {
+        "displayName": "Cloud Function - Active instances",
+        "conditionThreshold": {
+          "filter": "resource.type = \"cloud_function\" AND metric.type = \"cloudfunctions.googleapis.com/function/active_instances\"",
+          "aggregations": [
+            {
+              "alignmentPeriod": "300s",
+              "crossSeriesReducer": "REDUCE_NONE",
+              "perSeriesAligner": "ALIGN_MEAN"
+            }
+          ],
+          "comparison": "COMPARISON_GT",
+          "duration": "0s",
+          "trigger": {
+            "count": 1
+          },
+          "thresholdValue": 1
+        }
+      }
+    ],
+    "alertStrategy": {
+      "autoClose": "604800s"
+    },
+    "combiner": "OR",
+    "enabled": true,
+    "notificationChannels": [],
+    "severity": "SEVERITY_UNSPECIFIED"
+  }
+EOF_END
+
+
+gcloud alpha monitoring policies create --policy-from-file="app-engine-error-percent-policy.json"
