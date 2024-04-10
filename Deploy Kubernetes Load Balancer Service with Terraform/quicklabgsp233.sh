77a7d82ce3c6b9ba4752279240590aaf907f1d40
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-04-10 16:05:14 +0530

    Add files via upload

---
+++ Deploy Kubernetes Load Balancer Service with Terraform/quicklabgsp233.sh
@@ -0,0 +1,12 @@
+
+export REGION="${ZONE%-*}"
+
+gsutil -m cp -r gs://spls/gsp233/* .
+
+cd tf-gke-k8s-service-lb
+
+terraform init
+
+terraform apply -var="region=$REGION" -var="location=$ZONE" --auto-approve
+
+
