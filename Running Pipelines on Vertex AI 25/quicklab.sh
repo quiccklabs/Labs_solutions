8ad7a063c41a3c359615ed5628018efa5554c175
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-04-10 15:51:18 +0530

    Update quicklab.sh

--- Running Pipelines on Vertex AI 25/quicklab.sh
+++ Running Pipelines on Vertex AI 25/quicklab.sh
@@ -8,17 +8,17 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
   --role=roles/storage.admin
 
 
-gcloud storage buckets create gs://qwiklabs-gcp-00-7c52a8670d3b
+gcloud storage buckets create gs://$DEVSHELL_PROJECT_ID
 touch emptyfile1
 touch emptyfile2
-gcloud storage cp emptyfile1 gs://qwiklabs-gcp-00-7c52a8670d3b/pipeline-output/emptyfile1
-gcloud storage cp emptyfile2 gs://qwiklabs-gcp-00-7c52a8670d3b/pipeline-input/emptyfile2
+gcloud storage cp emptyfile1 gs://$DEVSHELL_PROJECT_ID/pipeline-output/emptyfile1
+gcloud storage cp emptyfile2 gs://$DEVSHELL_PROJECT_ID/pipeline-input/emptyfile2
 
 
 wget https://storage.googleapis.com/cloud-training/dataengineering/lab_assets/ai_pipelines/basic_pipeline.json
 
-sed -i 's/PROJECT_ID/qwiklabs-gcp-00-7c52a8670d3b/g' basic_pipeline.json
+sed -i 's/PROJECT_ID/$DEVSHELL_PROJECT_ID/g' basic_pipeline.json
 
 tail -20 basic_pipeline.json
 
-gcloud storage cp basic_pipeline.json gs://qwiklabs-gcp-00-7c52a8670d3b/pipeline-input/basic_pipeline.json
+gcloud storage cp basic_pipeline.json gs://$DEVSHELL_PROJECT_ID/pipeline-input/basic_pipeline.json
