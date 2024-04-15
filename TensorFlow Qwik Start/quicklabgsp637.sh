b32241662e98f94519e194da94a309cc9bf86313
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-04-15 19:08:19 +0530

    Add files via upload

---
+++ TensorFlow Qwik Start/quicklabgsp637.sh
@@ -0,0 +1,39 @@
+
+
+pip install google-cloud-logging
+
+pip install ---upgrade protobuf
+
+pip install --upgrade tensorflow
+
+python --version
+
+python -c "import tensorflow;print(tensorflow.__version__)"
+
+
+cat > model.py <<'EOF'
+import logging
+import google.cloud.logging as cloud_logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+from google.cloud.logging_v2.handlers import setup_logging
+
+cloud_logger = logging.getLogger('cloudLogger')
+cloud_logger.setLevel(logging.INFO)
+cloud_logger.addHandler(CloudLoggingHandler(cloud_logging.Client()))
+cloud_logger.addHandler(logging.StreamHandler())
+
+import tensorflow as tf
+import numpy as np
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+
+model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
+
+model.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.MeanSquaredError())
+
+model.fit(xs, ys, epochs=500)
+cloud_logger.info(str(model.predict([10.0])))
+EOF
+
+python model.py
