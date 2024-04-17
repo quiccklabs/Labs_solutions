1b967f05bff8a8b6385fa7e57b22c170dcb006eb
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-04-18 00:20:27 +0530

    Add files via upload

---
+++ Configuring an HTTP Load Balancer with Autoscaling Azure/quicklab.sh
@@ -0,0 +1,286 @@
+
+
+
+export REGION="${ZONE%-*}"
+
+
+
+gcloud compute --project=$DEVSHELL_PROJECT_ID firewall-rules create fw-allow-health-checks --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=130.211.0.0/22,35.191.0.0/16 --target-tags=allow-health-checks
+
+
+gcloud compute routers create nat-router-us1 \
+    --network=default \
+    --region=$REGION
+
+
+    gcloud compute routers nats create nat-config \
+    --router=nat-router-us1 \
+    --auto-allocate-nat-external-ips \
+    --nat-all-subnet-ip-ranges \
+    --region=$REGION
+
+
+sleep 60
+
+
+gcloud compute instances create webserver --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=stack-type=IPV4_ONLY,subnet=default,no-address --metadata=startup-script=sudo\ apt-get\ update$'\n'sudo\ apt-get\ install\ -y\ apache2$'\n'sudo\ update-rc.d\ apache2\ enable$'\n'sudo\ service\ apache2\ status,enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=allow-health-checks --create-disk=boot=yes,device-name=webserver,image=projects/debian-cloud/global/images/debian-10-buster-v20240312,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced  --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+
+
+gcloud compute instances stop webserver --zone=$ZONE --quiet
+
+gcloud compute images create mywebserver --project=$DEVSHELL_PROJECT_ID --source-disk=webserver --source-disk-zone=$ZONE --storage-location=us
+
+gcloud compute instances start webserver --zone=$ZONE --quiet
+
+
+gcloud beta compute instance-templates create mywebserver-template --project=$DEVSHELL_PROJECT_ID --machine-type=f1-micro --network-interface=network=default,no-address --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=allow-health-checks --create-disk=auto-delete=yes,boot=yes,device-name=mywebserver-template,image=projects/$DEVSHELL_PROJECT_ID/global/images/mywebserver,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
+
+gcloud beta compute health-checks create tcp http-health-check --project=$DEVSHELL_PROJECT_ID --port=80 --proxy-header=NONE --no-enable-logging --check-interval=5 --timeout=5 --unhealthy-threshold=2 --healthy-threshold=2
+
+
+gcloud beta compute instance-groups managed create us-central1-mig --project=$DEVSHELL_PROJECT_ID --base-instance-name=us-central1-mig --size=1 --template=projects/$DEVSHELL_PROJECT_ID/global/instanceTemplates/mywebserver-template --zones=$REGION-c,$REGION-b,$REGION-a --target-distribution-shape=EVEN --instance-redistribution-type=PROACTIVE --list-managed-instances-results=PAGELESS --health-check=projects/$DEVSHELL_PROJECT_ID/global/healthChecks/http-health-check --initial-delay=60 --no-force-update-on-repair --default-action-on-vm-failure=repair --standby-policy-mode=manual && gcloud beta compute instance-groups managed set-autoscaling us-central1-mig --project=$DEVSHELL_PROJECT_ID --region=$REGION --cool-down-period=60 --max-num-replicas=2 --min-num-replicas=1 --mode=on --target-load-balancing-utilization=0.8
+
+
+gcloud beta compute instance-groups managed create europe-west1-mig --project=$DEVSHELL_PROJECT_ID --base-instance-name=europe-west1-mig --size=1 --template=projects/$DEVSHELL_PROJECT_ID/global/instanceTemplates/mywebserver-template --region=$REGION_2 --target-distribution-shape=EVEN --instance-redistribution-type=PROACTIVE --list-managed-instances-results=PAGELESS --health-check=projects/$DEVSHELL_PROJECT_ID/global/healthChecks/http-health-check --initial-delay=60 --no-force-update-on-repair --default-action-on-vm-failure=repair --standby-policy-mode=manual && gcloud beta compute instance-groups managed set-autoscaling europe-west1-mig --project=$DEVSHELL_PROJECT_ID --region=$REGION_2 --cool-down-period=60 --max-num-replicas=2 --min-num-replicas=1 --mode=on --target-load-balancing-utilization=0.8
+
+
+DEVSHELL_PROJECT_ID=$(gcloud config get-value project)
+TOKEN=$(gcloud auth application-default print-access-token)
+
+# Define cURL command for creating security policy
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+    "description": "Default security policy for: http-backend",
+    "name": "default-security-policy-for-backend-service-http-backend",
+    "rules": [
+      {
+        "action": "allow",
+        "match": {
+          "config": {
+            "srcIpRanges": [
+              "*"
+            ]
+          },
+          "versionedExpr": "SRC_IPS_V1"
+        },
+        "priority": 2147483647
+      },
+      {
+        "action": "throttle",
+        "description": "Default rate limiting rule",
+        "match": {
+          "config": {
+            "srcIpRanges": [
+              "*"
+            ]
+          },
+          "versionedExpr": "SRC_IPS_V1"
+        },
+        "priority": 2147483646,
+        "rateLimitOptions": {
+          "conformAction": "allow",
+          "enforceOnKey": "IP",
+          "exceedAction": "deny(403)",
+          "rateLimitThreshold": {
+            "count": 500,
+            "intervalSec": 60
+          }
+        }
+      }
+    ]
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/securityPolicies"
+
+sleep 30
+
+# Define cURL command for creating backend services
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+    "backends": [
+      {
+        "balancingMode": "RATE",
+        "capacityScaler": 1,
+        "group": "projects/'"$DEVSHELL_PROJECT_ID"'/regions/'"$REGION"'/instanceGroups/us-central1-mig",
+        "maxRatePerInstance": 50
+      },
+      {
+        "balancingMode": "UTILIZATION",
+        "capacityScaler": 1,
+        "group": "projects/'"$DEVSHELL_PROJECT_ID"'/regions/'"$REGION_2"'/instanceGroups/europe-west1-mig",
+        "maxUtilization": 0.8
+      }
+    ],
+    "cdnPolicy": {
+      "cacheKeyPolicy": {
+        "includeHost": true,
+        "includeProtocol": true,
+        "includeQueryString": true
+      },
+      "cacheMode": "CACHE_ALL_STATIC",
+      "clientTtl": 3600,
+      "defaultTtl": 3600,
+      "maxTtl": 86400,
+      "negativeCaching": false,
+      "serveWhileStale": 0
+    },
+    "compressionMode": "DISABLED",
+    "connectionDraining": {
+      "drainingTimeoutSec": 300
+    },
+    "description": "",
+    "enableCDN": true,
+    "healthChecks": [
+      "projects/'"$DEVSHELL_PROJECT_ID"'/global/healthChecks/http-health-check"
+    ],
+    "loadBalancingScheme": "EXTERNAL_MANAGED",
+    "localityLbPolicy": "ROUND_ROBIN",
+    "logConfig": {
+      "enable": true,
+      "sampleRate": 1
+    },
+    "name": "http-backend",
+    "portName": "http",
+    "protocol": "HTTP",
+    "securityPolicy": "projects/'"$DEVSHELL_PROJECT_ID"'/global/securityPolicies/default-security-policy-for-backend-service-http-backend",
+    "sessionAffinity": "NONE",
+    "timeoutSec": 30
+  }' \
+  "https://compute.googleapis.com/compute/beta/projects/$DEVSHELL_PROJECT_ID/global/backendServices"
+
+sleep 30
+
+  # Define cURL command for setting security policy for backend service
+  curl -X POST -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TOKEN" \
+    -d '{
+      "securityPolicy": "projects/'"$DEVSHELL_PROJECT_ID"'/global/securityPolicies/default-security-policy-for-backend-service-http-backend"
+    }' \
+    "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/backendServices/http-backend/setSecurityPolicy"
+
+sleep 30
+
+
+# Define cURL command for creating URL map
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+    "defaultService": "projects/'"$DEVSHELL_PROJECT_ID"'/global/backendServices/http-backend",
+    "name": "http-lb"
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/urlMaps"
+
+sleep 30
+
+# Define cURL command for creating target HTTP proxy
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+    "name": "http-lb-target-proxy",
+    "urlMap": "projects/'"$DEVSHELL_PROJECT_ID"'/global/urlMaps/http-lb"
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/targetHttpProxies"
+
+sleep 30
+
+# Define cURL command for creating forwarding rule (IPv4)
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+    "IPProtocol": "TCP",
+    "ipVersion": "IPV4",
+    "loadBalancingScheme": "EXTERNAL_MANAGED",
+    "name": "http-lb-forwarding-rule",
+    "networkTier": "PREMIUM",
+    "portRange": "80",
+    "target": "projects/'"$DEVSHELL_PROJECT_ID"'/global/targetHttpProxies/http-lb-target-proxy"
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/forwardingRules"
+
+sleep 30
+
+# Define cURL command for creating target HTTP proxy (IPv6)
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+    "name": "http-lb-target-proxy-2",
+    "urlMap": "projects/'"$DEVSHELL_PROJECT_ID"'/global/urlMaps/http-lb"
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/targetHttpProxies"
+
+sleep 30
+
+# Define cURL command for creating forwarding rule (IPv6)
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+    "IPProtocol": "TCP",
+    "ipVersion": "IPV6",
+    "loadBalancingScheme": "EXTERNAL_MANAGED",
+    "name": "http-lb-forwarding-rule-2",
+    "networkTier": "PREMIUM",
+    "portRange": "80",
+    "target": "projects/'"$DEVSHELL_PROJECT_ID"'/global/targetHttpProxies/http-lb-target-proxy-2"
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/forwardingRules"
+
+sleep 30
+
+  curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+    "namedPorts": [
+      {
+        "name": "http",
+        "port": 80
+      }
+    ]
+  }' \
+  "https://compute.googleapis.com/compute/beta/projects/$DEVSHELL_PROJECT_ID/regions/$REGION_2/instanceGroups/europe-west1-mig/setNamedPorts"
+
+sleep 30
+
+# Define cURL command for setting named ports for instance group (US region)
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -d '{
+    "namedPorts": [
+      {
+        "name": "http",
+        "port": 80
+      }
+    ]
+  }' \
+  "https://compute.googleapis.com/compute/beta/projects/$DEVSHELL_PROJECT_ID/regions/$REGION/instanceGroups/us-central1-mig/setNamedPorts"
+
+
+
+
+
+
+
+LB_IP_ADDRESS=$(gcloud compute forwarding-rules describe http-lb-forwarding-rule --global --format="value(IPAddress)")
+
+
+
+
+LB_IP=$LB_IP_ADDRESS
+while [ -z "$RESULT" ] ;
+do
+  echo "Waiting for Load Balancer";
+  sleep 5;
+  RESULT=$(curl -m1 -s $LB_IP | grep Apache);
+done
+
+
+
+gcloud compute instances create stress-test --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-micro --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --create-disk=auto-delete=yes,boot=yes,device-name=stress-test,image=projects/$DEVSHELL_PROJECT_ID/global/images/mywebserver,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+
+
+
+
+
+gcloud compute ssh --zone "$ZONE" "stress-test" --project "$DEVSHELL_PROJECT_ID" --quiet --command "sudo apt-get -y install siege && export LB_IP=$LB_IP_ADDRESS && siege -c 150 -t 120s http://\$LB_IP"
+
+
