3745c91ef1dd2d74b5d76c3ed324b5af23a1e6b9
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-06-06 20:22:17 +0530

    Add files via upload

---
+++ Introduction to Computer Vision with TensorFlow 2024/quicklabgsp631.sh
@@ -0,0 +1,35 @@
+
+python --version
+
+python -c "import tensorflow;print(tensorflow.__version__)"
+
+pip install tensorflow tensorflow-datasets google-cloud-logging numpy
+
+pip3 install --upgrade pip
+
+/usr/bin/python3 -m pip install -U google-cloud-logging --user
+
+/usr/bin/python3 -m pip install -U pylint --user
+
+pip install --upgrade tensorflow
+
+
+wget  https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow%202024/model.py
+
+wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow%202024/callback_model.py
+
+wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow%202024/updated_model_1.py
+
+wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow%202024/updated_model_2.py
+
+wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow%202024/updated_model_3.py
+
+python model.py
+
+python callback_model.py
+
+python updated_model_1.py
+
+python updated_model_2.py
+
+python updated_model_3.py
