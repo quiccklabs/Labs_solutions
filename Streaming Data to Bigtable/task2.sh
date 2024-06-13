cdf6ece13f6524693cc927ecea7270f604dd66bf
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-06-14 02:56:20 +0530

    Update task2.sh

--- Streaming Data to Bigtable/task2.sh
+++ Streaming Data to Bigtable/task2.sh
@@ -13,14 +13,3 @@ gcloud compute scp prepare_disk_1.sh training-vm:/tmp --project=$DEVSHELL_PROJEC
 gcloud compute ssh training-vm --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk_1.sh"
 
 
-echo "Listing all Dataflow jobs:"
-gcloud dataflow jobs list
-
-
-export JOB_ID=$(gcloud dataflow jobs list | grep JOB_ID: | awk '{print $2}')
-
-gcloud dataflow jobs cancel $JOB_ID
-
-cbt deletetable current_conditions
-
-echo -e "\e[1m\e[34mhttps://console.cloud.google.com/dataflow/jobs?referrer=search&project=$DEVSHELL_PROJECT_ID\e[0m"
