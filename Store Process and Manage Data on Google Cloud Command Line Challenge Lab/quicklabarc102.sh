f26acd071c110400022135a51233ccb85936b37a
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-06-19 01:00:02 +0530

    Add files via upload

---
+++ Store Process and Manage Data on Google Cloud Command Line Challenge Lab/quicklabarc102.sh
@@ -0,0 +1,172 @@
+
+gcloud config set compute/region $REGION
+export PROJECT_ID=$(gcloud config get-value project)
+
+
+gcloud services enable \
+  artifactregistry.googleapis.com \
+  cloudfunctions.googleapis.com \
+  cloudbuild.googleapis.com \
+  eventarc.googleapis.com \
+  run.googleapis.com \
+  logging.googleapis.com \
+  pubsub.googleapis.com
+
+
+
+gsutil mb -l $REGION gs://$BUCKET_NAME
+
+
+gcloud pubsub topics create $TOPIC_NAME
+
+
+
+PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$PROJECT_ID" --format='value(project_number)')
+SERVICE_ACCOUNT=$(gsutil kms serviceaccount -p $PROJECT_NUMBER)
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member serviceAccount:$SERVICE_ACCOUNT \
+  --role roles/pubsub.publisher
+  
+
+mkdir ~/quicklab && cd $_
+touch index.js && touch package.json
+
+
+cat > index.js <<'EOF_END'
+/* globals exports, require */
+//jshint strict: false
+//jshint esversion: 6
+"use strict";
+const crc32 = require("fast-crc32c");
+const { Storage } = require('@google-cloud/storage');
+const gcs = new Storage();
+const { PubSub } = require('@google-cloud/pubsub');
+const imagemagick = require("imagemagick-stream");
+
+exports.thumbnail = (event, context) => {
+  const fileName = event.name;
+  const bucketName = event.bucket;
+  const size = "64x64"
+  const bucket = gcs.bucket(bucketName);
+  const topicName = "REPLACE_WITH_YOUR_TOPIC ID";
+  const pubsub = new PubSub();
+  if ( fileName.search("64x64_thumbnail") == -1 ){
+    // doesn't have a thumbnail, get the filename extension
+    var filename_split = fileName.split('.');
+    var filename_ext = filename_split[filename_split.length - 1];
+    var filename_without_ext = fileName.substring(0, fileName.length - filename_ext.length );
+    if (filename_ext.toLowerCase() == 'png' || filename_ext.toLowerCase() == 'jpg'){
+      // only support png and jpg at this point
+      console.log(`Processing Original: gs://${bucketName}/${fileName}`);
+      const gcsObject = bucket.file(fileName);
+      let newFilename = filename_without_ext + size + '_thumbnail.' + filename_ext;
+      let gcsNewObject = bucket.file(newFilename);
+      let srcStream = gcsObject.createReadStream();
+      let dstStream = gcsNewObject.createWriteStream();
+      let resize = imagemagick().resize(size).quality(90);
+      srcStream.pipe(resize).pipe(dstStream);
+      return new Promise((resolve, reject) => {
+        dstStream
+          .on("error", (err) => {
+            console.log(`Error: ${err}`);
+            reject(err);
+          })
+          .on("finish", () => {
+            console.log(`Success: ${fileName} → ${newFilename}`);
+              // set the content-type
+              gcsNewObject.setMetadata(
+              {
+                contentType: 'image/'+ filename_ext.toLowerCase()
+              }, function(err, apiResponse) {});
+              pubsub
+                .topic(topicName)
+                .publisher()
+                .publish(Buffer.from(newFilename))
+                .then(messageId => {
+                  console.log(`Message ${messageId} published.`);
+                })
+                .catch(err => {
+                  console.error('ERROR:', err);
+                });
+          });
+      });
+    }
+    else {
+      console.log(`gs://${bucketName}/${fileName} is not an image I can handle`);
+    }
+  }
+  else {
+    console.log(`gs://${bucketName}/${fileName} already has a thumbnail`);
+  }
+};
+EOF_END
+
+
+sed -i '16c\  const topicName = "'$TOPIC_NAME'";' index.js
+
+
+
+cat > package.json <<EOF_END
+{
+    "name": "thumbnails",
+    "version": "1.0.0",
+    "description": "Create Thumbnail of uploaded image",
+    "scripts": {
+      "start": "node index.js"
+    },
+    "dependencies": {
+      "@google-cloud/pubsub": "^2.0.0",
+      "@google-cloud/storage": "^5.0.0",
+      "fast-crc32c": "1.0.4",
+      "imagemagick-stream": "4.1.1"
+    },
+    "devDependencies": {},
+    "engines": {
+      "node": ">=4.3.2"
+    }
+  }
+EOF_END
+
+
+
+
+  
+  deploy_function () {
+    gcloud functions deploy $FUNCTION_NAME \
+      --gen2 \
+      --runtime nodejs20 \
+      --entry-point thumbnail \
+      --source . \
+      --region $REGION \
+      --trigger-bucket $BUCKET_NAME \
+      --allow-unauthenticated \
+      --trigger-location $REGION \
+      --max-instances 5 \
+      --quiet
+  }
+    
+    # Variables
+    SERVICE_NAME="$FUNCTION_NAME"
+    
+    # Loop until the Cloud Run service is created
+    while true; do
+      # Run the deployment command
+      deploy_function
+    
+      # Check if Cloud Run service is created
+      if gcloud run services describe $SERVICE_NAME --region $REGION &> /dev/null; then
+        echo "Cloud Run service is created. Exiting the loop."
+        break
+      else
+        echo "Waiting for Cloud Run service to be created..."
+        echo "Meantime Subscribe to Quicklab[https://www.youtube.com/@quick_lab]."
+        sleep 10
+      fi
+    done
+
+
+
+wget https://storage.googleapis.com/cloud-training/arc102/wildlife.jpg
+
+gsutil cp wildlife.jpg gs://$BUCKET_NAME
