71921d4ad0db923c14b873c7b79afccae81e945d
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-06-22 00:34:20 +0530

    Add files via upload

---
+++ Build an AI Image Generator app using Imagen on Vertex AI/quicklab.sh
@@ -0,0 +1,44 @@
+
+
+cat > GenerateImage.py <<EOF_END
+import argparse
+
+import vertexai
+from vertexai.preview.vision_models import ImageGenerationModel
+
+def generate_image(
+    project_id: str, location: str, output_file: str, prompt: str
+) -> vertexai.preview.vision_models.ImageGenerationResponse:
+    """Generate an image using a text prompt.
+    Args:
+      project_id: Google Cloud project ID, used to initialize Vertex AI.
+      location: Google Cloud region, used to initialize Vertex AI.
+      output_file: Local path to the output image file.
+      prompt: The text prompt describing what you want to see."""
+
+    vertexai.init(project=project_id, location=location)
+
+    model = ImageGenerationModel.from_pretrained("imagegeneration@002")
+
+    images = model.generate_images(
+        prompt=prompt,
+        # Optional parameters
+        number_of_images=1,
+        seed=1,
+        add_watermark=False,
+    )
+
+    images[0].save(location=output_file)
+
+    return images
+
+generate_image(
+    project_id='qwiklabs-gcp-01-b411e09cba7c',
+    location='us-central1',
+    output_file='image.jpeg',
+    prompt='Create an image of a cricket ground in the heart of Los Angeles',
+    )
+
+EOF_END
+
+/usr/bin/python3 /home/student/GenerateImage.py
