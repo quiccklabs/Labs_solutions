1ae0e9e0281719710589079e22a134ea5726ab60
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-06-23 00:11:36 +0530

    Add files via upload

---
+++ Build an AI Image Recognition app using Gemini on Vertex AI/quicklab.sh
@@ -0,0 +1,41 @@
+
+
+ID="$(gcloud projects list --format='value(PROJECT_ID)')"
+
+cat > genai.py <<EOF_END
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part
+
+
+def generate_text(project_id: str, location: str) -> str:
+    # Initialize Vertex AI
+    vertexai.init(project=project_id, location=location)
+    # Load the model
+    multimodal_model = GenerativeModel("gemini-1.0-pro-vision")
+    # Query the model
+    response = multimodal_model.generate_content(
+        [
+            # Add an example image
+            Part.from_uri(
+                "gs://generativeai-downloads/images/scones.jpg", mime_type="image/jpeg"
+            ),
+            # Add an example query
+            "what is shown in this image?",
+        ]
+    )
+
+    return response.text
+
+# --------  Important: Variable declaration  --------
+
+project_id = "$ID"
+location = "$REGION"
+
+#  --------   Call the Function  --------
+
+response = generate_text(project_id, location)
+print(response)
+
+EOF_END
+
+/usr/bin/python3 /home/student/genai.py
