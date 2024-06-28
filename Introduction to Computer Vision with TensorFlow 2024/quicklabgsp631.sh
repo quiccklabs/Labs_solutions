fc01b72f9c0610186574b5c04cb3c9a77a7252bb
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-06-29 00:32:19 +0530

    Update quicklabgsp631.sh

--- Introduction to Computer Vision with TensorFlow 2024/quicklabgsp631.sh
+++ Introduction to Computer Vision with TensorFlow 2024/quicklabgsp631.sh
@@ -24,6 +24,8 @@ wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduc
 
 wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow%202024/updated_model_3.py
 
+wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow%202024/update_model_4.py
+
 python model.py
 
 python callback_model.py
@@ -34,4 +36,19 @@ python updated_model_2.py
 
 python updated_model_3.py
 
+sleep 20
+
+pip uninstall numpy -y
+
+pip uninstall numpy tensorflow -y
+
+pip install tensorflow==2.10.0 numpy==1.21.6
+
+pip uninstall google-cloud-logging google-api-core google-protobuf -y
+
+pip install google-cloud-logging==2.6.0 google-api-core==1.31.5 protobuf==3.20.1
+
+pip show google-cloud-logging google-api-core google-protobuf
+
+python update_model_4.py
 
