e5f252a297d5eab07c418a129b4748d884ab2bae
quiccklabs <108678347+quiccklabs@users.noreply.github.com>
2024-07-14 01:26:41 +0530

    Add files via upload

---
+++ Develop an app with Gemini/quicklab.sh
@@ -0,0 +1,16 @@
+
+
+
+PROJECT_ID=$(gcloud config get-value project)
+
+echo "PROJECT_ID=${PROJECT_ID}"
+echo "REGION=${REGION}"
+
+USER=$(gcloud config get-value account 2> /dev/null)
+echo "USER=${USER}"
+
+gcloud services enable cloudaicompanion.googleapis.com --project ${PROJECT_ID}
+gcloud services enable run.googleapis.com
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/cloudaicompanion.user
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/serviceusage.serviceUsageViewer
