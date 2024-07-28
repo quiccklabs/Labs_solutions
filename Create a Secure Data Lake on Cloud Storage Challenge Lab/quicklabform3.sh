ba045e692c7cf9252693b2aa6d2a3239f8faacd5
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-07-28 07:52:44 +0530

    Create quicklabform3.sh

---
+++ Create a Secure Data Lake on Cloud Storage Challenge Lab/quicklabform3.sh
@@ -0,0 +1,32 @@
+
+
+
+#TASK 1
+bq mk --location=US Raw_data
+
+bq load --source_format=AVRO Raw_data.public-data gs://spls/gsp1145/users.avro
+
+
+# TASK 2
+
+
+gcloud dataplex zones create temperature-raw-data \
+    --lake=public-lake \
+    --location=$REGION \
+    --type=RAW \
+    --resource-location-type=SINGLE_REGION \
+    --display-name="temperature-raw-data"
+
+#TASK 3
+
+
+gcloud dataplex assets create customer-details-dataset \
+    --location=$REGION \
+    --lake=public-lake \
+    --zone=temperature-raw-data \
+    --resource-type=BIGQUERY_DATASET \
+    --resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customer_reference_data \
+    --display-name="Customer Details Dataset" \
+    --discovery-enabled  
+
+
