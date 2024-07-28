66b31ed7e8bc5c24b46defbaab04a797af51d5a7
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-07-29 00:20:23 +0530

    Add files via upload

---
+++ Create a Secure Data Lake on Cloud Storage Challenge Lab/quicklab.sh
@@ -0,0 +1,62 @@
+
+
+
+
+export REGION="${ZONE%-*}"
+
+#TASK 1
+gsutil mb -p $DEVSHELL_PROJECT_ID -l $REGION -b on gs://$DEVSHELL_PROJECT_ID-bucket/
+
+
+#TASK 2
+gcloud alpha dataplex lakes create customer-lake \
+--display-name="Customer-Lake" \
+ --location=$REGION \
+ --labels="key_1=$KEY_1,value_1=$VALUE_1"
+
+
+gcloud dataplex zones create public-zone \
+    --lake=customer-lake \
+    --location=$REGION \
+    --type=RAW \
+    --resource-location-type=SINGLE_REGION \
+    --display-name="Public-Zone"
+
+
+gcloud dataplex environments create dataplex-lake-env \
+           --project=$DEVSHELL_PROJECT_ID --location=$REGION --lake=customer-lake \
+           --os-image-version=1.0 --compute-node-count 3  --compute-max-node-count 3 
+
+
+#TASK3
+
+gcloud dataplex assets create customer-raw-data --location=$REGION \
+            --lake=customer-lake --zone=public-zone \
+            --resource-type=STORAGE_BUCKET \
+            --resource-name=projects/$DEVSHELL_PROJECT_ID/buckets/$DEVSHELL_PROJECT_ID-customer-bucket \
+            --discovery-enabled \
+            --display-name="Customer Raw Data"
+
+
+gcloud dataplex assets create customer-reference-data --location=$REGION \
+            --lake=customer-lake --zone=public-zone \
+            --resource-type=BIGQUERY_DATASET \
+            --resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customer_reference_data \
+            --display-name="Customer Reference Data"
+
+#TASK 4
+
+
+# gcloud data-catalog tag-templates create customer_data_tag_template \
+#    --project=$DEVSHELL_PROJECT_ID \
+#    --location=$REGION \
+#    --display-name="Customer Data Tag Template"
+#    --field=id=DataOwner,display-name=Data\ Owner,type=string,required=TRUE \
+#    --field=id=PIIData,display-name=PII\ Data,type='enum(Yes|No)',required=TRUE
+
+# gcloud data-catalog tag-templates create customer_data_tag_template \
+#     --project=$DEVSHELL_PROJECT_ID \
+#     --location=$REGION \
+#     --display-name="Customer Data Tag Template" \
+#     --field=id=DataOwner,type=string,display-name="Data Owner" \
+#     --field=id=PIIData,type='enum(Yes|No)',display-name="PII Data"
