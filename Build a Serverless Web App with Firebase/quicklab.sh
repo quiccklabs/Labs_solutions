04ff955fa16eab25f80d729d33da9a096b4a1656
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-07-29 18:04:35 +0530

    Add files via upload

---
+++ Build a Serverless Web App with Firebase/quicklab.sh
@@ -0,0 +1,10 @@
+
+git clone https://github.com/rosera/pet-theory.git
+
+cd pet-theory/lab02/
+
+npm i && npm audit fix --force
+
+gcloud firestore databases create --location=$REGION
+
+echo https://console.cloud.google.com/firebase?referrer=search&project=$DEVSHELL_PROJECT_ID
