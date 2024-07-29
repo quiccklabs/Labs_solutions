4ca1e783b4d30a15f9944b0445df2a45d2897f83
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-07-29 10:36:17 +0530

    Update quicklabarc106.sh

--- Streaming Analytics into BigQuery Challenge Lab/quicklabarc106.sh
+++ Streaming Analytics into BigQuery Challenge Lab/quicklabarc106.sh
@@ -3,6 +3,7 @@
 
 
 
+
 gsutil mb gs://$DEVSHELL_PROJECT_ID
 
 bq mk $DATASET_NAME
@@ -48,4 +49,30 @@ gcloud pubsub topics publish $TOPIC_NAME --message='{"data": "73.4 F"}'
 bq query --nouse_legacy_sql "SELECT * FROM \`$DEVSHELL_PROJECT_ID.$DATASET_NAME.$TABLE_NAME\`"
 
 
+
+gcloud dataflow jobs run $JOB_NAME-quicklab --gcs-location gs://dataflow-templates-$REGION/latest/PubSub_to_BigQuery --region $REGION --staging-location gs://$DEVSHELL_PROJECT_ID/temp --parameters inputTopic=projects/$DEVSHELL_PROJECT_ID/topics/$TOPIC_NAME,outputTableSpec=$DEVSHELL_PROJECT_ID:$DATASET_NAME.$TABLE_NAME
+
+
+while true; do
+    STATUS=$(gcloud dataflow jobs list --region="$REGION" --filter="NAME:$JOB_NAME-quicklab AND STATE:Running" --format='value(STATE)')
+
+    
+    if [ "$STATUS" == "Running" ]; then
+        # Run the next code here 
+        echo "The Dataflow job is running. Proceeding with the next code."
+        # Add your next code here
+
+        sleep 20
+        gcloud pubsub topics publish $TOPIC_NAME --message='{"data": "73.4 F"}'
+
+        bq query --nouse_legacy_sql "SELECT * FROM \`$DEVSHELL_PROJECT_ID.$DATASET_NAME.$TABLE_NAME\`"
+        break  # exit the loop since the job is running
+    else
+        # Wait for a certain amount of time before checking again
+        sleep 30  # you can adjust the sleep duration based on your needs
+        echo "The Dataflow job is not running. Waiting for it to start..."
+        echo "Meantime Like Share & Subscribe to Quicklab[https://www.youtube.com/@quick_lab]..."
+        # Continue to the next iteration of the loop
+    fi
+done
 
