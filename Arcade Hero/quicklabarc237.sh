c508d11f1d433346c35b51010dc2c7a3d3cedf72
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-08-05 18:53:35 +0530

    Create quicklabarc237.sh

---
+++ Arcade Hero/quicklabarc237.sh
@@ -0,0 +1,15 @@
+gcloud config set compute/region $REGION
+export PROJECT_ID=$(gcloud config get-value project)
+
+gcloud pubsub topics create cf-demo
+git clone https://github.com/GoogleCloudPlatform/nodejs-docs-samples.git
+cd nodejs-docs-samples/functions/v2/helloPubSub/
+
+gcloud functions deploy cf-demo \
+--gen2 \
+--runtime=nodejs20 \
+--region=$REGION \
+--source=. \
+--entry-point=helloPubSub \
+--trigger-topic=cf_topic \
+--quiet
