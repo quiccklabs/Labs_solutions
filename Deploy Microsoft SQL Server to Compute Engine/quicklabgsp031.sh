c44ada254b59a8a5d9161c9f640dfd166b750b89
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-08-06 02:35:07 +0530

    Add files via upload

---
+++ Deploy Microsoft SQL Server to Compute Engine/quicklabgsp031.sh
@@ -0,0 +1,7 @@
+
+
+
+
+gcloud compute instances create sqlserver-lab --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append --create-disk=auto-delete=yes,boot=yes,device-name=sqlserver-lab,image=projects/windows-sql-cloud/global/images/sql-2016-web-windows-2016-dc-v20240711,mode=rw,size=50,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+
+gcloud compute reset-windows-password sqlserver-lab --zone=$ZONE --quiet
