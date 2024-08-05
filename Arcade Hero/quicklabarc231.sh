67c8328d1e52fd1d9356376b4979eff314661c23
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-08-05 17:07:59 +0530

    Update quicklabarc231.sh

--- Arcade Hero/quicklabarc231.sh
+++ Arcade Hero/quicklabarc231.sh
@@ -4,30 +4,29 @@ export PROJECT_ID=$(gcloud config get-value project)
 
 mkdir quicklab && cd quicklab
 
-cat > index.js <<EOF_END
+cat > index.js <<'EOF_END'
 const functions = require('@google-cloud/functions-framework');
 
-functions.http('helloHttp', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+// Register an HTTP function with the Functions Framework that will be executed
+// when you make an HTTP request to the deployed function's endpoint.
+functions.http('helloGET', (req, res) => {
+  res.send('Subscribe to Quicklab!');
 });
-
 EOF_END
 
 cat > package.json <<EOF_END
-{
-  "dependencies": {
-    "@google-cloud/functions-framework": "^3.0.0"
-  }
+"dependencies": {
+  "@google-cloud/functions-framework": "^3.1.0"
 }
 
 EOF_END
 
 gcloud functions deploy cf-demo \
   --gen2 \
-  --runtime nodejs20 \
-  --entry-point helloHttp \    
-  --source . \
-  --region $REGION \
+  --runtime=nodejs20 \
+  --region=$REGION \
+  --source=. \
+  --entry-point=helloGET \
   --trigger-http \
   --allow-unauthenticated \
   --quiet
