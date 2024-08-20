d58a26cfffec160b9aef4d390603b20d1cfe87c4
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-08-20 23:03:33 +0530

    Add files via upload

---
+++ Configuring Using and Auditing VM Service Accounts and Scopes/quicklab.sh
@@ -0,0 +1,121 @@
+
+
+
+gcloud iam service-accounts create my-sa-123 \
+    --display-name "My Service Account"
+
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member="serviceAccount:my-sa-123@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/editor"
+
+
+#TASK 2
+
+gcloud iam service-accounts create bigquery-qwiklabs \
+    --description="Service account for BigQuery access" \
+    --display-name="bigquery-qwiklabs"
+
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member="serviceAccount:bigquery-qwiklabs@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/bigquery.dataViewer"
+
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member="serviceAccount:bigquery-qwiklabs@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/bigquery.user"
+
+
+
+gcloud iam service-accounts delete bigquery-qwiklabs@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --quiet
+
+
+gcloud iam service-accounts create bigquery-qwiklab \
+    --description="Service account for BigQuery access" \
+    --display-name="bigquery-qwiklab"
+
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member="serviceAccount:bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/bigquery.dataViewer"
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member="serviceAccount:bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/bigquery.user"
+
+
+sleep 20
+
+gcloud compute instances create bigquery-instance \
+    --project=$DEVSHELL_PROJECT_ID \
+    --zone=$ZONE \
+    --machine-type=e2-medium \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --metadata=enable-oslogin=true \
+    --maintenance-policy=MIGRATE \
+    --provisioning-model=STANDARD \
+    --service-account=bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/bigquery,https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
+    --create-disk=auto-delete=yes,boot=yes,device-name=bigquery-instance,image=projects/debian-cloud/global/images/debian-11-bullseye-v20240815,mode=rw,size=10,type=pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
+
+
+
+
+
+cat > prepare_disk.sh <<'EOF_END'
+sudo apt-get update
+sudo apt-get install -y git python3-pip
+pip3 install --upgrade pip
+pip3 install google-cloud-bigquery
+pip3 install pyarrow
+pip3 install pandas
+pip3 install db-dtypes
+echo "
+from google.auth import compute_engine
+from google.cloud import bigquery
+
+credentials = compute_engine.Credentials(
+    service_account_email='YOUR_SERVICE_ACCOUNT')
+
+query = '''
+SELECT
+  year,
+  COUNT(1) as num_babies
+FROM
+  publicdata.samples.natality
+WHERE
+  year > 2000
+GROUP BY
+  year
+'''
+
+client = bigquery.Client(
+    project='qwiklabs-gcp-04-1d9c399303d7',
+    credentials=credentials)
+print(client.query(query).to_dataframe())
+" > query.py
+
+sed -i -e "s/qwiklabs-gcp-04-1d9c399303d7/$(gcloud config get-value project)/g" query.py
+sed -i -e "s/YOUR_SERVICE_ACCOUNT/bigquery-qwiklab@$(gcloud config get-value project).iam.gserviceaccount.com/g" query.py
+python3 query.py
+EOF_END
+
+
+# Copy the environment variables script to the VM
+
+# Copy the prepare_disk.sh script to the VM
+gcloud compute scp prepare_disk.sh bigquery-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+# SSH into the VM and execute the script
+gcloud compute ssh bigquery-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
+# Copy the prepare_disk.sh script to the VM
+gcloud compute scp prepare_disk.sh bigquery-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+# SSH into the VM and execute the script
+gcloud compute ssh bigquery-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
