d2586ee4ddf4f4e5f4623460bae2f7e5133a13e6
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-09-02 21:33:04 +0530

    Update quicklabgsp646.sh

--- Clean Up Unused IP Addresses/quicklabgsp646.sh
+++ Clean Up Unused IP Addresses/quicklabgsp646.sh
@@ -2,6 +2,14 @@
 
 
 
+
+gcloud services disable cloudfunctions.googleapis.com
+
+gcloud services enable cloudfunctions.googleapis.com
+
+
+
+
 PROJECT_ID=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
 REGION="${ZONE%-*}"
 USED_IP=used-ip-address
@@ -27,7 +35,38 @@ gcloud compute instances create static-ip-instance \
 --address=$USED_IP_ADDRESS
 sleep 20
 
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+--member="serviceAccount:$DEVSHELL_PROJECT_ID@appspot.gserviceaccount.com" \
+--role="roles/artifactregistry.reader"
+
+
+
+PROJECT_NUMBER=$(gcloud projects describe $DEVSHELL_PROJECT_ID --format='value(projectNumber)')
+
+# Your existing deployment command
+deploy_function() {
 gcloud functions deploy unused_ip_function --trigger-http --runtime=nodejs12 --region=$REGION --quiet
+}
+
+# Variables
+SERVICE_NAME="unused_ip_function"
+
+# Loop until the Cloud Function is deployed
+while true; do
+  # Run the deployment command
+  deploy_function
+
+  # Check if Cloud Function is deployed
+  if gcloud functions describe $SERVICE_NAME --region $REGION &> /dev/null; then
+    echo "Cloud Function is deployed. Exiting the loop."
+    break
+  else
+    echo "Waiting for Cloud Function to be deployed..."
+    echo "Meantime Subscribe to Quicklab[https://www.youtube.com/@quick_lab]."
+    sleep 10
+  fi
+done
+
 
 export FUNCTION_URL=$(gcloud functions describe unused_ip_function --region=$REGION --format=json | jq -r '.httpsTrigger.url')
 
@@ -43,9 +82,15 @@ gcloud scheduler jobs create http unused-ip-job \
 --location=$REGION \
 --oidc-service-account-email=$DEVSHELL_PROJECT_ID@appspot.gserviceaccount.com
 
+sleep 60
+
 gcloud scheduler jobs run unused-ip-job \
 --location=$REGION
+
+
 gcloud compute addresses list --filter="region:($REGION)"
+
+
  
 WORKDIR=$(pwd)
 cd $WORKDIR/gcf-automated-resource-cleanup/unused-ip
@@ -54,7 +99,6 @@ REGION="${ZONE%-*}"
 USED_IP=used-ip-address
 UNUSED_IP=unused-ip-address
 gcloud compute addresses create $UNUSED_IP --project=$PROJECT_ID --region=$REGION
-
 
 
 
