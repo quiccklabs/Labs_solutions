e2873c5e4b3eb00b0ca0e530cec5c426db9cde18
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-09-05 01:02:50 +0530

    Add files via upload

---
+++ Create and Manage AlloyDB Instances Challenge Lab/quicklabgsp395.sh
@@ -0,0 +1,23 @@
+
+
+gcloud beta alloydb clusters create lab-cluster \
+    --password=Change3Me \
+    --network=peering-network \
+    --region=$REGION \
+    --project=$DEVSHELL_PROJECT_ID
+
+gcloud beta alloydb instances create lab-instance \
+    --instance-type=PRIMARY \
+    --cpu-count=2 \
+    --region=$REGION  \
+    --cluster=lab-cluster \
+    --project=$DEVSHELL_PROJECT_ID
+
+gcloud alloydb instances create lab-instance-rp1 \
+  --cluster=lab-cluster \
+  --region=$REGION \
+  --instance-type=READ_POOL \
+  --cpu-count=2 \
+  --read-pool-node-count=2
+
+gcloud beta alloydb backups create lab-backup --region=$REGION  --cluster=lab-cluster
