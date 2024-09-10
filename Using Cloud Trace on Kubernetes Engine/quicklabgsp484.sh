f0c212cd62a81fec241bd87390920b61e64c557b
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-09-10 23:11:09 +0530

    Add files via upload

---
+++ Using Cloud Trace on Kubernetes Engine/quicklabgsp484.sh
@@ -0,0 +1,39 @@
+
+
+gcloud auth list 
+
+git clone https://github.com/GoogleCloudPlatform/gke-tracing-demo
+cd gke-tracing-demo/terraform
+
+rm provider.tf
+
+cat > provider.tf <<EOF_END
+provider "google" {
+  project = var.project
+}
+EOF_END
+
+terraform init
+
+../scripts/generate-tfvars.sh
+
+
+terraform plan -var="project=$DEVSHELL_PROJECT_ID" -var="zone=$ZONE"
+
+
+cat > terraform.tfvars <<EOF_END
+project = $DEVSHELL_PROJECT_ID
+zone    = $ZONE
+EOF_END
+
+terraform apply -var="project=$DEVSHELL_PROJECT_ID" -var="zone=$ZONE" -auto-approve
+
+kubectl apply -f tracing-demo-deployment.yaml
+
+sleep 30
+
+echo http://$(kubectl get svc tracing-demo -n default -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+sleep 30
+
+echo http://$(kubectl get svc tracing-demo -n default -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
