77c22c1d251d5774ec51f630270b6ce9ca79bd3e
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-09-17 01:57:13 +0530

    Create task1.sh

---
+++ Speech to Text Transcription with the Cloud Speech API/task1.sh
@@ -0,0 +1,36 @@
+cat > prepare_disk.sh <<'EOF_END'
+
+gcloud services enable apikeys.googleapis.com
+
+gcloud alpha services api-keys create --display-name="quicklab" 
+
+KEY=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=quicklab")
+
+API_KEY=$(gcloud alpha services api-keys get-key-string $KEY --format="value(keyString)")
+
+cat > request.json <<EOF
+
+{
+  "config": {
+      "encoding":"FLAC",
+      "languageCode": "en-US"
+  },
+  "audio": {
+      "uri":"gs://cloud-samples-data/speech/brooklyn_bridge.flac"
+  }
+}
+
+EOF
+
+curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json \
+"https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" > result.json
+
+cat result.json
+
+EOF_END
+
+export ZONE=$(gcloud compute instances list linux-instance --format 'csv[no-heading](zone)')
+
+gcloud compute scp prepare_disk.sh linux-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh linux-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
