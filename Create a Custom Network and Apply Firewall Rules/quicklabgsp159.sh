a6575ab85ff971104f85a4e9e18eac5313e12d83
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-09-18 00:44:59 +0530

    Update quicklabgsp159.sh

--- Create a Custom Network and Apply Firewall Rules/quicklabgsp159.sh
+++ Create a Custom Network and Apply Firewall Rules/quicklabgsp159.sh
@@ -1,15 +1,12 @@
+# Prompt user to input three regions
+read -p "Enter REGION_1: " REGION_1
+read -p "Enter REGION_2: " REGION_2
+read -p "Enter REGION_3: " REGION_3
 
-
-
-
-read -p "REGION_1"
-read -p "REGION_2"
-read -p "REGION_3"
-
-
-
+# Create a custom network
 gcloud compute networks create taw-custom-network --subnet-mode custom
 
+# Create subnets in the specified regions with the correct IP ranges
 gcloud compute networks subnets create subnet-$REGION_1 \
    --network taw-custom-network \
    --region $REGION_1 \
@@ -25,17 +22,24 @@ gcloud compute networks subnets create subnet-$REGION_3 \
    --region $REGION_3 \
    --range 10.2.0.0/16
 
+# List the created subnets in the custom network
 gcloud compute networks subnets list \
    --network taw-custom-network
 
+# Create firewall rules for HTTP, ICMP, internal traffic, SSH, and RDP
 gcloud compute firewall-rules create nw101-allow-http \
---allow tcp:80 --network taw-custom-network --source-ranges 0.0.0.0/0 \
---target-tags http
+   --allow tcp:80 --network taw-custom-network --source-ranges 0.0.0.0/0 \
+   --target-tags http
 
-gcloud compute firewall-rules create "nw101-allow-icmp" --allow icmp --network "taw-custom-network" --target-tags rules
+gcloud compute firewall-rules create nw101-allow-icmp \
+   --allow icmp --network taw-custom-network --target-tags rules
 
-gcloud compute firewall-rules create "nw101-allow-internal" --allow tcp:0-65535,udp:0-65535,icmp --network "taw-custom-network" --source-ranges "10.0.0.0/16","10.2.0.0/16","10.1.0.0/16"
+gcloud compute firewall-rules create nw101-allow-internal \
+   --allow tcp:0-65535,udp:0-65535,icmp --network taw-custom-network \
+   --source-ranges "10.0.0.0/16","10.2.0.0/16","10.1.0.0/16"
 
-gcloud compute firewall-rules create "nw101-allow-ssh" --allow tcp:22 --network "taw-custom-network" --target-tags "ssh"
+gcloud compute firewall-rules create nw101-allow-ssh \
+   --allow tcp:22 --network taw-custom-network --target-tags ssh
 
-gcloud compute firewall-rules create "nw101-allow-rdp" --allow tcp:3389 --network "taw-custom-network"
+gcloud compute firewall-rules create nw101-allow-rdp \
+   --allow tcp:3389 --network taw-custom-network
