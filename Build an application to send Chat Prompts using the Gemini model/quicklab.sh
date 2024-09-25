0d1d21af1ef0519df952554ee9dc9ace8e71449e
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-09-26 01:16:25 +0530

    Update quicklab.sh

--- Build an application to send Chat Prompts using the Gemini model/quicklab.sh
+++ Build an application to send Chat Prompts using the Gemini model/quicklab.sh
@@ -8,6 +8,14 @@ cat > SendChatwithoutStream.py <<EOF
 import vertexai
 from vertexai.generative_models import GenerativeModel, ChatSession
 
+import logging
+from google.cloud import logging as gcp_logging
+
+# ------  Below cloud logging code is for Qwiklab's internal use, do not edit/remove it. --------
+# Initialize GCP logging
+gcp_logging_client = gcp_logging.Client()
+gcp_logging_client.setup_logging()
+
 project_id = "$ID"
 location = "$REGION"
 
@@ -16,7 +24,9 @@ model = GenerativeModel("gemini-1.0-pro")
 chat = model.start_chat()
 
 def get_chat_response(chat: ChatSession, prompt: str) -> str:
+    logging.info(f'Sending prompt: {prompt}')
     response = chat.send_message(prompt)
+    logging.info(f'Received response: {response.text}')
     return response.text
 
 prompt = "Hello."
@@ -27,6 +37,7 @@ print(get_chat_response(chat, prompt))
 
 prompt = "Why does it appear when it rains?"
 print(get_chat_response(chat, prompt))
+
 EOF
 
 /usr/bin/python3 /home/student/SendChatwithoutStream.py
@@ -34,6 +45,14 @@ EOF
 cat > SendChatwithStream.py <<EOF
 import vertexai
 from vertexai.generative_models import GenerativeModel, ChatSession
+
+import logging
+from google.cloud import logging as gcp_logging
+
+# ------  Below cloud logging code is for Qwiklab's internal use, do not edit/remove it. --------
+# Initialize GCP logging
+gcp_logging_client = gcp_logging.Client()
+gcp_logging_client.setup_logging()
 
 project_id = "$ID"
 location = "$REGION"
@@ -44,10 +63,12 @@ chat = model.start_chat()
 
 def get_chat_response(chat: ChatSession, prompt: str) -> str:
     text_response = []
+    logging.info(f'Sending prompt: {prompt}')
     responses = chat.send_message(prompt, stream=True)
     for chunk in responses:
         text_response.append(chunk.text)
     return "".join(text_response)
+    logging.info(f'Received response: {response.text}')
 
 prompt = "Hello."
 print(get_chat_response(chat, prompt))
@@ -57,6 +78,7 @@ print(get_chat_response(chat, prompt))
 
 prompt = "Why does it appear when it rains?"
 print(get_chat_response(chat, prompt))
+
 EOF
 
 
