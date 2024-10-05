0bfe4cd33faf626e6881f3c90341e5e27455992b
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-10-05 23:43:27 +0530

    Create quicklabarc115.sh

---
+++ Monitoring in Google Cloud Challenge Lab/quicklabarc115.sh
@@ -0,0 +1,112 @@
+ZONE="$(gcloud compute instances list --project=$DEVSHELL_PROJECT_ID --format='value(ZONE)' | head -n 1)"
+
+INSTANCE_ID=$(gcloud compute instances describe apache-vm --zone=$ZONE --format='value(id)')
+
+#TASK 1
+
+
+cat > prepare_disk.sh <<'EOF_END'
+curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+sudo bash add-logging-agent-repo.sh --also-install
+
+curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
+sudo bash add-monitoring-agent-repo.sh --also-install
+
+(cd /etc/stackdriver/collectd.d/ && sudo curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/collectd.d/apache.conf)
+
+sudo service stackdriver-agent restart
+EOF_END
+
+gcloud compute scp prepare_disk.sh apache-vm:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh apache-vm --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
+
+
+
+
+#TASK 2
+
+
+
+gcloud monitoring uptime create quicklab \
+  --resource-type="gce-instance" \
+  --resource-labels=project_id=$DEVSHELL_PROJECT_ID,instance_id=$INSTANCE_ID,zone=$ZONE
+
+
+#TASK 3
+
+cat > email-channel.json <<EOF_END
+{
+  "type": "email",
+  "displayName": "quicklab",
+  "description": "Subscribe to quicklab",
+  "labels": {
+    "email_address": "$USER_EMAIL"
+  }
+}
+EOF_END
+
+
+gcloud beta monitoring channels create --channel-content-from-file="email-channel.json"
+
+
+# Run the gcloud command and store the output in a variable
+channel_info=$(gcloud beta monitoring channels list)
+
+# Extract the channel ID using grep and awk
+channel_id=$(echo "$channel_info" | grep -oP 'name: \K[^ ]+' | head -n 1)
+
+
+cat > app-engine-error-percent-policy.json <<EOF_END
+
+{
+  "displayName": "alert",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "VM Instance - Traffic",
+      "conditionThreshold": {
+        "filter": "resource.type = \"gce_instance\" AND metric.type = \"agent.googleapis.com/apache/traffic\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "60s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_RATE"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "0s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": 3072
+      }
+    }
+  ],
+  "alertStrategy": {
+    "autoClose": "1800s"
+  },
+  "combiner": "OR",
+  "enabled": true,
+  "notificationChannels": [
+    "$channel_id"
+  ],
+  "severity": "SEVERITY_UNSPECIFIED"
+}
+
+EOF_END
+
+
+gcloud alpha monitoring policies create --policy-from-file="app-engine-error-percent-policy.json"
+
+
+
+# ANSI escape codes for bold and green
+BOLD_GREEN="\033[1;32m"
+BOLD_RED="\033[1;32m"
+RESET="\033[0m"
+
+# Print the URL in bold and green
+echo -e "Open Dashboard: ${BOLD_GREEN}https://console.cloud.google.com/workstations/list?project=${RESET}"
+
+echo -e "Create Metric: ${BOLD_GREEN}https://console.cloud.google.com/workstations/list?project=${RESET}"
