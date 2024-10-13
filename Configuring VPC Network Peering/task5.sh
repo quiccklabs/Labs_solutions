8163ccf49a965e8237bc27ca016849b6c4db040a
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-10-14 03:21:39 +0530

    Update task5.sh

--- Configuring VPC Network Peering/task5.sh
+++ Configuring VPC Network Peering/task5.sh
@@ -1,5 +1,7 @@
 #TASK 5
 
+ZONE=$(gcloud compute instances list --format="value(ZONE)" | tail -n 2 | head -n 1)
+
 gcloud compute networks peerings delete peering-1-2 --network=mynetwork
 
 INTERNAL_IP_privatenet=$(gcloud compute instances describe privatenet-us-vm --zone=$ZONE --format="get(networkInterfaces[0].networkIP)")
