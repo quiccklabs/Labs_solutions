6d3f5dfc6cef007c7dfc2035b07576f0a8f851c7
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-10-14 03:23:38 +0530

    Update task4.sh

--- Configuring VPC Network Peering/task4.sh
+++ Configuring VPC Network Peering/task4.sh
@@ -3,6 +3,10 @@ ZONE=$(gcloud compute instances list --format="value(ZONE)" | tail -n 2 | head -
 
 INTERNAL_IP_mynet_us_vm=$(gcloud compute instances describe mynet-us-vm --zone=$ZONE --format="get(networkInterfaces[0].networkIP)")
 
+BOLD_GREEN="\033[1;32m"
+BOLD_RED="\033[1;31m"  # Changed to bold red
+RESET="\033[0m"
+
 echo -e "${BOLD_RED}INTERNAL_IP :- ${BOLD_GREEN}$INTERNAL_IP_mynet_us_vm${RESET}"
 
 gcloud compute ssh --zone "$ZONE" "privatenet-us-vm" --project "$DEVSHELL_PROJECT_ID" --quiet
