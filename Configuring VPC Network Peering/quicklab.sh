611297957d48c85a62b364c209a9aa8e3bec8914
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-10-14 02:47:22 +0530

    Create quicklab.sh

---
+++ Configuring VPC Network Peering/quicklab.sh
@@ -0,0 +1,18 @@
+
+
+
+curl -LO https://raw.githubusercontent.com/quiccklabs/Labs_solutions/refs/heads/master/Configuring%20VPC%20Network%20Peering/task1.sh
+
+curl -LO https://raw.githubusercontent.com/quiccklabs/Labs_solutions/refs/heads/master/Configuring%20VPC%20Network%20Peering/task2.sh
+
+curl -LO https://raw.githubusercontent.com/quiccklabs/Labs_solutions/refs/heads/master/Configuring%20VPC%20Network%20Peering/task3.sh
+
+curl -LO https://raw.githubusercontent.com/quiccklabs/Labs_solutions/refs/heads/master/Configuring%20VPC%20Network%20Peering/task4.sh
+
+curl -LO https://raw.githubusercontent.com/quiccklabs/Labs_solutions/refs/heads/master/Configuring%20VPC%20Network%20Peering/task5.sh
+
+chmod +x task1.sh
+chmod +x task2.sh
+chmod +x task3.sh
+chmod +x task4.sh
+chmod +x task5.sh
