d25049d2ca72b1c0d2615b628dbecea959b4f2f2
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-10-22 23:26:25 +0530

    Create quicklab.sh

---
+++ AUGUST Serverless Firebase Development Challenge Lab/quicklab.sh
@@ -0,0 +1,62 @@
+
+echo ""
+echo ""
+echo "Please export the values."
+
+
+# Prompt user to input three regions
+read -p "Enter REGION: " REGION
+
+
+export SERVICE_NAME=netflix-dataset-service
+
+export FRONTEND_STAGING_SERVICE_NAME=frontend-staging-service
+
+export FRONTEND_PRODUCTION_SERVICE_NAME=frontend-production-service
+
+
+
+gcloud config set project $(gcloud projects list --format='value(PROJECT_ID)' --filter='qwiklabs-gcp')
+
+gcloud services enable run.googleapis.com
+
+gcloud firestore databases create --location=$REGION
+
+
+git clone https://github.com/rosera/pet-theory.git
+
+cd pet-theory/lab06/firebase-import-csv/solution
+npm install
+node index.js netflix_titles_original.csv
+
+
+cd ~/pet-theory/lab06/firebase-rest-api/solution-01
+npm install
+gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.1
+gcloud beta run deploy $SERVICE_NAME --image gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.1 --allow-unauthenticated --region=$REGION
+
+cd ~/pet-theory/lab06/firebase-rest-api/solution-02
+npm install
+gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.2
+gcloud beta run deploy $SERVICE_NAME --image gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.2 --allow-unauthenticated --region=$REGION
+
+
+SERVICE_URL=$(gcloud run services describe $SERVICE_NAME --platform=managed --region=$REGION --format="value(status.url)")
+
+curl -X GET $SERVICE_URL/2019
+
+cd ~/pet-theory/lab06/firebase-frontend/public
+
+sed -i 's/^const REST_API_SERVICE = "data\/netflix\.json"/\/\/ const REST_API_SERVICE = "data\/netflix.json"/' app.js
+
+sed -i "1i const REST_API_SERVICE = \"$SERVICE_URL/2020\"" app.js
+
+
+npm install
+cd ~/pet-theory/lab06/firebase-frontend
+gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1
+gcloud beta run deploy $FRONTEND_STAGING_SERVICE_NAME --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1 --region=$REGION --quiet
+
+
+gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-production:0.1
+gcloud beta run deploy $FRONTEND_PRODUCTION_SERVICE_NAME --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-production:0.1 --region=$REGION --quiet
