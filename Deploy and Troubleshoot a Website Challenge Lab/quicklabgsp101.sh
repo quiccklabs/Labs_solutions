21b9baf077fc2389b6e37532c3d6dea51b4f496e
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-10-23 12:15:51 +0530

    Update quicklabgsp101.sh

--- Deploy and Troubleshoot a Website Challenge Lab/quicklabgsp101.sh
+++ Deploy and Troubleshoot a Website Challenge Lab/quicklabgsp101.sh
@@ -1,10 +1,17 @@
 
 
 
+echo ""
+echo ""
+echo "Please export the values."
 
+
+# Prompt user to input three regions
+read -p "Enter INSTANCE_NAME: " INSTANCE_NAME
+
+read -p "Enter ZONE: " ZONE
 
 gcloud compute instances create $INSTANCE_NAME --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=f1-micro --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script=sudo\ su\ -$'\n'$'\n'apt-get\ update$'\n'apt-get\ install\ apache2\ -y$'\n'$'\n'service\ --status-all$'\n',enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=http-server,https-server --create-disk=auto-delete=yes,boot=yes,device-name=$INSTANCE_NAME,image=projects/debian-cloud/global/images/debian-12-bookworm-v20240312,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
-
 
 IP=$(gcloud compute instances list $INSTANCE_NAME --zones=$ZONE --format='value(EXTERNAL_IP)')
 
