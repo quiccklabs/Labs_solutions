89189bcf89b67985fb8cd206d2a04f88ffcbf534
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2024-12-06 01:12:39 +0530

    Create quicklabbig1.sh

---
+++ mini lab/quicklabbig1.sh
@@ -0,0 +1,16 @@
+echo ""
+echo ""
+echo "Please export the values."
+
+
+# Prompt user to input three regions
+read -p "Enter BUCKET_NAME: " BUCKET
+
+
+bq mk work_day
+
+bq mk --table work_day.employee \
+    employee_id:INTEGER,device_id:STRING,username:STRING,department:STRING,office:STRING
+
+bq load --source_format=CSV --skip_leading_rows=1 work_day.employee gs://$BUCKET/employees.csv employee_id:INTEGER,device_id:STRING,username:STRING,department:STRING,office:STRING
+
