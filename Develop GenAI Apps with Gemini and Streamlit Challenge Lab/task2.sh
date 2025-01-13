017038ff8dc2ef9dbf863eff259eb00dc6ee9e7d
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-01-13 19:40:30 +0530

    Create task2.sh

---
+++ Develop GenAI Apps with Gemini and Streamlit Challenge Lab/task2.sh
@@ -0,0 +1,14 @@
+AR_REPO='chef-repo'
+SERVICE_NAME='chef-streamlit-app' 
+gcloud artifacts repositories create "$AR_REPO" --location="$REGION" --repository-format=Docker
+gcloud builds submit --tag "$REGION-docker.pkg.dev/$PROJECT/$AR_REPO/$SERVICE_NAME"
+
+
+gcloud run deploy "$SERVICE_NAME" \
+  --port=8080 \
+  --image="$REGION-docker.pkg.dev/$PROJECT/$AR_REPO/$SERVICE_NAME" \
+  --allow-unauthenticated \
+  --region=$REGION \
+  --platform=managed  \
+  --project=$PROJECT \
+  --set-env-vars=GCP_PROJECT=$PROJECT,GCP_REGION=$REGION
