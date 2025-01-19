e0ab49c5ac2c9286b3d745997a0ed6e54dab3cd4
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-01-20 02:12:26 +0530

    Create task1.sh

---
+++ Build a RAG Solution with Firebase Genkit/task1.sh
@@ -0,0 +1,42 @@
+echo ""
+echo ""
+
+read -p "ENTER REGION:- " REGION
+
+
+export GCLOUD_PROJECT=$DEVSHELL_PROJECT_ID
+export GCLOUD_LOCATION=$REGION
+
+
+gcloud auth list 
+
+mkdir genkit-intro && cd genkit-intro
+npm init -y
+
+npm install -D genkit-cli@1.0.0-rc.2
+
+npm install genkit@1.0.0-rc.2 --save
+npm install @genkit-ai/vertexai@1.0.0-rc.2 @genkit-ai/google-cloud@1.0.0-rc.2 express --save
+npm install @genkit-ai/dev-local-vectorstore@1.0.0-rc.2 @genkit-ai/evaluator@1.0.0-rc.2 --save
+
+mkdir src && touch src/index.ts
+
+## add file 
+
+wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/refs/heads/master/Build%20a%20RAG%20Solution%20with%20Firebase%20Genkit/index.ts
+
+sed -i "s/us-west1/$REGION/g" index.ts
+
+gcloud storage cp ~/genkit-intro/package.json gs://$GCLOUD_PROJECT
+gcloud storage cp ~/genkit-intro/src/index.ts gs://$GCLOUD_PROJECT
+
+## edit file 
+
+
+gcloud storage cp ~/genkit-intro/src/index.ts gs://$GCLOUD_PROJECT
+
+
+## TASK 3
+
+cd ~/genkit-intro
+npx genkit start -- npx tsx src/index.ts | tee -a response_output.json
