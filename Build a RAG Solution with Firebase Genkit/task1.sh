8dd39d8519b62d609575cea6e44573fede3c4174
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-01-20 02:29:04 +0530

    Update task1.sh

--- Build a RAG Solution with Firebase Genkit/task1.sh
+++ Build a RAG Solution with Firebase Genkit/task1.sh
@@ -19,13 +19,15 @@ npm install genkit@1.0.0-rc.2 --save
 npm install @genkit-ai/vertexai@1.0.0-rc.2 @genkit-ai/google-cloud@1.0.0-rc.2 express --save
 npm install @genkit-ai/dev-local-vectorstore@1.0.0-rc.2 @genkit-ai/evaluator@1.0.0-rc.2 --save
 
-mkdir src && touch src/index.ts
+mkdir src && cd src
 
 ## add file 
 
 wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/refs/heads/master/Build%20a%20RAG%20Solution%20with%20Firebase%20Genkit/index.ts
 
 sed -i "s/us-west1/$REGION/g" index.ts
+
+cd ..
 
 gcloud storage cp ~/genkit-intro/package.json gs://$GCLOUD_PROJECT
 gcloud storage cp ~/genkit-intro/src/index.ts gs://$GCLOUD_PROJECT
