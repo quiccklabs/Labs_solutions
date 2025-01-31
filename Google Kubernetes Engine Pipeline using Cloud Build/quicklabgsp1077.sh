88e22a2ccd69c96c894f2f724616de105272a2a8
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-01-31 07:02:06 +0530

    Update quicklabgsp1077.sh

--- Google Kubernetes Engine Pipeline using Cloud Build/quicklabgsp1077.sh
+++ Google Kubernetes Engine Pipeline using Cloud Build/quicklabgsp1077.sh
@@ -1,6 +1,8 @@
 
 
 
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
 export PROJECT_ID=$(gcloud config get-value project)
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
@@ -9,7 +11,7 @@ gcloud config set compute/region $REGION
 
 gcloud services enable container.googleapis.com \
     cloudbuild.googleapis.com \
-    sourcerepo.googleapis.com \
+    secretmanager.googleapis.com \
     containeranalysis.googleapis.com
 
 
@@ -19,18 +21,24 @@ gcloud artifacts repositories create my-repository \
 
 gcloud container clusters create hello-cloudbuild --num-nodes 1 --region $REGION
 
+curl -sS https://webi.sh/gh | sh 
+gh auth login 
+gh api user -q ".login"
+GITHUB_USERNAME=$(gh api user -q ".login")
+git config --global user.name "${GITHUB_USERNAME}"
+git config --global user.email "${USER_EMAIL}"
+echo ${GITHUB_USERNAME}
+echo ${USER_EMAIL}
 
-git config --global user.email "$USER_EMAIL"
 
-git config --global user.name "quicklab"
+gh repo create  hello-cloudbuild-app --private 
 
-gcloud source repos create hello-cloudbuild-app
-
-gcloud source repos create hello-cloudbuild-env
+gh repo create  hello-cloudbuild-env --private
 
 cd ~
+mkdir hello-cloudbuild-app
 
-git clone https://github.com/GoogleCloudPlatform/gke-gitops-tutorial-cloudbuild hello-cloudbuild-app
+gcloud storage cp -r gs://spls/gsp1077/gke-gitops-tutorial-cloudbuild/* hello-cloudbuild-app
 
 cd ~/hello-cloudbuild-app
 
@@ -42,22 +50,18 @@ sed -i "s/us-central1/$REGION/g" kubernetes.yaml.tpl
 
 PROJECT_ID=$(gcloud config get-value project)
 
-git remote add google "https://source.developers.google.com/p/${PROJECT_ID}/r/hello-cloudbuild-app"
+git init
+git config credential.helper gcloud.sh
+git remote add google https://github.com/${GITHUB_USERNAME}/hello-cloudbuild-app
+git branch -m master
+git add . && git commit -m "initial commit"
+
 
 cd ~/hello-cloudbuild-app
 
 COMMIT_ID="$(git rev-parse --short=7 HEAD)"
 
 gcloud builds submit --tag="${REGION}-docker.pkg.dev/${PROJECT_ID}/my-repository/hello-cloudbuild:${COMMIT_ID}" .
-
-
-gcloud builds triggers create cloud-source-repositories \
-    --name="hello-cloudbuild" \
-    --description="subscribe to quicklab" \
-    --service-account="projects/$PROJECT_ID/serviceAccounts/$PROJECT_ID@$PROJECT_ID.iam.gserviceaccount.com" \
-    --repo="hello-cloudbuild-app" \
-    --branch-pattern="^master$" \
-    --build-config="cloudbuild.yaml"
 
 cd ~/hello-cloudbuild-app
 
@@ -67,23 +71,73 @@ git commit -m "Type Any Commit Message here"
 
 git push google master
 
-PROJECT_NUMBER="$(gcloud projects describe ${PROJECT_ID} --format='get(projectNumber)')"
+cd ~
+
+mkdir workingdir
+cd workingdir
+
+
+ssh-keygen -t rsa -b 4096 -N '' -f id_github -C "${USER_EMAIL}"
+
+gcloud secrets create ssh_key_secret --replication-policy="automatic"
+
+gcloud secrets versions add ssh_key_secret --data-file=id_github
+
+
+GITHUB_TOKEN=$(gh auth token)
+
+SSH_KEY_CONTENT=$(cat ~/workingdir/id_github.pub)
+
+gh api --method POST -H "Accept: application/vnd.github.v3+json" \
+  /repos/${GITHUB_USERNAME}/hello-cloudbuild-env/keys \
+  -f title="SSH_KEY" \
+  -f key="$SSH_KEY_CONTENT" \
+  -F read_only=false
+
+rm id_github*
+
+gcloud projects add-iam-policy-binding ${PROJECT_NUMBER} \
+--member=serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
+--role=roles/secretmanager.secretAccessor
+
+cd ~
 
 gcloud projects add-iam-policy-binding ${PROJECT_NUMBER} \
 --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
 --role=roles/container.developer
 
-cd ~
 
-gcloud source repos clone hello-cloudbuild-env
+mkdir hello-cloudbuild-env
+gcloud storage cp -r gs://spls/gsp1077/gke-gitops-tutorial-cloudbuild/* hello-cloudbuild-env
 
-cd ~/hello-cloudbuild-env
+
+cd hello-cloudbuild-env
+sed -i "s/us-central1/$REGION/g" cloudbuild.yaml
+sed -i "s/us-central1/$REGION/g" cloudbuild-delivery.yaml
+sed -i "s/us-central1/$REGION/g" cloudbuild-trigger-cd.yaml
+sed -i "s/us-central1/$REGION/g" kubernetes.yaml.tpl
+
+ssh-keyscan -t rsa github.com > known_hosts.github
+chmod +x known_hosts.github
+
+git init
+git config credential.helper gcloud.sh
+git remote add google https://github.com/${GITHUB_USERNAME}/hello-cloudbuild-env
+git branch -m master
+git add . && git commit -m "initial commit"
+git push google master
+
 
 git checkout -b production
 
-cd ~/hello-cloudbuild-env
+rm cloudbuild.yaml
 
-cp ~/hello-cloudbuild-app/cloudbuild-delivery.yaml ~/hello-cloudbuild-env/cloudbuild.yaml
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/refs/heads/main/Google%20Kubernetes%20Engine%20Pipeline%20using%20Cloud%20Build/ENV-cloudbuild.yaml
+
+mv ENV-cloudbuild.yaml cloudbuild.yaml
+
+sed -i "s/REGION-/$REGION/g" cloudbuild.yaml
+sed -i "s/GITHUB-USERNAME/${GITHUB_USERNAME}/g" cloudbuild.yaml
 
 git add .
 
@@ -91,41 +145,30 @@ git commit -m "Create cloudbuild.yaml for deployment"
 
 git checkout -b candidate
 
-git push origin production
+git push google production
 
-git push origin candidate
-
-PROJECT_NUMBER="$(gcloud projects describe ${PROJECT_ID} \
---format='get(projectNumber)')"
-cat >/tmp/hello-cloudbuild-env-policy.yaml <<EOF
-bindings:
-- members:
-  - serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com
-  role: roles/source.writer
-EOF
-
-
-gcloud source repos set-iam-policy \
-hello-cloudbuild-env /tmp/hello-cloudbuild-env-policy.yaml
-
-
-gcloud builds triggers create cloud-source-repositories \
-    --name="hello-cloudbuild-deploy" \
-    --description="subscribe to quicklab" \
-    --service-account="projects/$PROJECT_ID/serviceAccounts/$PROJECT_ID@$PROJECT_ID.iam.gserviceaccount.com" \
-    --repo="hello-cloudbuild-env" \
-    --branch-pattern="^candidate$" \
-    --build-config="cloudbuild.yaml"
-
+git push google candidate
 
 cd ~/hello-cloudbuild-app
+ssh-keyscan -t rsa github.com > known_hosts.github
+chmod +x known_hosts.github
 
-cp cloudbuild-trigger-cd.yaml cloudbuild.yaml
+git add .
+git commit -m "Adding known_host file."
+git push google master
 
-cd ~/hello-cloudbuild-app
+rm cloudbuild.yaml
+
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/refs/heads/main/Google%20Kubernetes%20Engine%20Pipeline%20using%20Cloud%20Build/APP-cloudbuild.yaml
+
+mv APP-cloudbuild.yaml cloudbuild.yaml
+
+sed -i "s/REGION/$REGION/g" cloudbuild.yaml
+sed -i "s/GITHUB-USERNAME/${GITHUB_USERNAME}/g" cloudbuild.yaml
 
 git add cloudbuild.yaml
 
 git commit -m "Trigger CD pipeline"
 
 git push google master
+
