e84ccacf4edb37a246306f5fc9affbabfbbd5ae2
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-02-04 20:29:02 +0530

    Create quicklabarc1201.sh

---
+++ Arcade Hero/quicklabarc1201.sh
@@ -0,0 +1,9 @@
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+PROJECT_ID=`gcloud config get-value project`
+
+bq mk --location=$REGION sports
