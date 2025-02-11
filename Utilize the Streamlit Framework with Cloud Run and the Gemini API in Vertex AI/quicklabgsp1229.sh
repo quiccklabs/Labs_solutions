7a3c84c29723c8ad3bd3664e1a34100c801a8af3
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-02-11 21:02:24 +0530

    Create quicklabgsp1229.sh

---
+++ Utilize the Streamlit Framework with Cloud Run and the Gemini API in Vertex AI/quicklabgsp1229.sh
@@ -0,0 +1,37 @@
+
+
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+PROJECT_ID=`gcloud config get-value project`
+
+
+git clone https://github.com/GoogleCloudPlatform/generative-ai.git --depth=1
+
+cd generative-ai/gemini/sample-apps/gemini-streamlit-cloudrun
+
+python3 -m venv gemini-streamlit
+source gemini-streamlit/bin/activate
+pip install -r requirements.txt
+
+
+GCP_PROJECT=$PROJECT_ID
+GCP_REGION=$REGION
+
+
+AR_REPO='gemini-repo'
+SERVICE_NAME='gemini-streamlit-app' 
+gcloud artifacts repositories create "$AR_REPO" --location="$GCP_REGION" --repository-format=Docker
+gcloud builds submit --tag "$GCP_REGION-docker.pkg.dev/$GCP_PROJECT/$AR_REPO/$SERVICE_NAME"
+
+gcloud run deploy "$SERVICE_NAME" \
+  --port=8080 \
+  --image="$GCP_REGION-docker.pkg.dev/$GCP_PROJECT/$AR_REPO/$SERVICE_NAME" \
+  --allow-unauthenticated \
+  --region=$GCP_REGION \
+  --platform=managed  \
+  --project=$GCP_PROJECT \
+  --set-env-vars=GCP_PROJECT=$GCP_PROJECT,GCP_REGION=$GCP_REGION
