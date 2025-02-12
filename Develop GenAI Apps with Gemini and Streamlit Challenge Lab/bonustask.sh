c50dfde75de3dc7d8c2f5b8a28561123b57f7ebd
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-02-13 02:12:46 +0530

    Update bonustask.sh

--- Develop GenAI Apps with Gemini and Streamlit Challenge Lab/bonustask.sh
+++ Develop GenAI Apps with Gemini and Streamlit Challenge Lab/bonustask.sh
@@ -2,9 +2,6 @@
 cd generative-ai/gemini/sample-apps/gemini-streamlit-cloudrun
 pip install google-cloud-aiplatform
 
-python3 -m venv gemini-streamlit
-source gemini-streamlit/bin/activate
-
 streamlit run chef.py \
   --browser.serverAddress=localhost \
   --server.enableCORS=false \
