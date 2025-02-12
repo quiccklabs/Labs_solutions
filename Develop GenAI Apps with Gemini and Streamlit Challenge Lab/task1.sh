509d636d854a9b7e5d152df4f1b257982bca4efd
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-02-13 00:48:57 +0530

    Update task1.sh

--- Develop GenAI Apps with Gemini and Streamlit Challenge Lab/task1.sh
+++ Develop GenAI Apps with Gemini and Streamlit Challenge Lab/task1.sh
@@ -22,14 +22,20 @@ gcloud storage cp chef.py gs://$DEVSHELL_PROJECT_ID-generative-ai/
 
 export PROJECT="$DEVSHELL_PROJECT_ID"
 
-
-python3 -m venv gemini-streamlit
-source gemini-streamlit/bin/activate
-python3 -m  pip install -r requirements.txt
+pip install google-cloud-aiplatform
 
 
-streamlit run chef.py \
-  --browser.serverAddress=localhost \
-  --server.enableCORS=false \
-  --server.enableXsrfProtection=false \
-  --server.port 8080
+curl -LO https://raw.githubusercontent.com/quiccklabs/Labs_solutions/refs/heads/master/Develop%20GenAI%20Apps%20with%20Gemini%20and%20Streamlit%20Challenge%20Lab/bonustask.sh
+sudo chmod +x bonustask.sh
+./bonustask.sh
+
+# python3 -m venv gemini-streamlit
+# source gemini-streamlit/bin/activate
+# python3 -m  pip install -r requirements.txt
+
+
+# streamlit run chef.py \
+#   --browser.serverAddress=localhost \
+#   --server.enableCORS=false \
+#   --server.enableXsrfProtection=false \
+#   --server.port 8080
