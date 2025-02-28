8f5d8a536988e822811ae21ce57c2d514b8341c5
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-03-01 00:33:06 +0530

    Update quicklabgsp602.sh

--- HTTP Google Cloud Functions in Go/quicklabgsp602.sh
+++ HTTP Google Cloud Functions in Go/quicklabgsp602.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+PROJECT_ID=`gcloud config get-value project`
+
+
+
+gcloud services enable run.googleapis.com
+
 gcloud services enable cloudfunctions.googleapis.com
 
 curl -LO https://github.com/GoogleCloudPlatform/golang-samples/archive/main.zip
@@ -8,6 +22,6 @@ unzip main.zip
 
 cd golang-samples-main/functions/codelabs/gopher
 
-gcloud functions deploy HelloWorld --runtime go120 --trigger-http --quiet
+gcloud functions deploy HelloWorld --gen2 --runtime go121 --trigger-http --region $REGION --quiet
 
-gcloud functions deploy Gopher --runtime go120 --trigger-http --quiet
+gcloud functions deploy Gopher --gen2 --runtime go121 --trigger-http --region $REGION --quiet
