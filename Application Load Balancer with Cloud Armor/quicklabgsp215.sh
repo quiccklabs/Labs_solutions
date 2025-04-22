3348c4469fcbf90fd9a84d39d1427195dd0b2261
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-04-22 23:38:32 +0530

    Update quicklabgsp215.sh

--- Application Load Balancer with Cloud Armor/quicklabgsp215.sh
+++ Application Load Balancer with Cloud Armor/quicklabgsp215.sh
@@ -122,7 +122,7 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/urlMaps"
 
-sleep 20
+sleep 40
 
 # Create Target HTTP Proxy
 curl -X POST -H "Content-Type: application/json" \
@@ -133,7 +133,7 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/targetHttpProxies"
 
-sleep 20
+sleep 40
 
 # Create Forwarding Rule
 curl -X POST -H "Content-Type: application/json" \
@@ -149,7 +149,7 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/forwardingRules"
 
-sleep 20
+sleep 40
 
 # Create another Target HTTP Proxy
 curl -X POST -H "Content-Type: application/json" \
@@ -160,7 +160,7 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/targetHttpProxies"
 
-sleep 20
+sleep 40
 
 
 # Create another Forwarding Rule
@@ -177,7 +177,7 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/forwardingRules"
 
-sleep 20
+sleep 40
 
 # Set Named Ports for Europe-West1 Instance Group
 curl -X POST -H "Content-Type: application/json" \
@@ -192,7 +192,7 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/regions/$REGION2/instanceGroups/$INSTANCE_NAME_2/setNamedPorts"
 
-sleep 20
+sleep 40
 
 # Set Named Ports for $REGION1 Instance Group
 curl -X POST -H "Content-Type: application/json" \
@@ -210,11 +210,11 @@ curl -X POST -H "Content-Type: application/json" \
 
 gcloud compute instances create siege-vm --project=$DEVSHELL_PROJECT_ID --zone=$VM_ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --create-disk=auto-delete=yes,boot=yes,device-name=siege-vm,image=projects/debian-cloud/global/images/debian-11-bullseye-v20230629,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/us-central1-c/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
 
-sleep 20
+sleep 40
 
 export EXTERNAL_IP=$(gcloud compute instances  describe siege-vm --zone=$VM_ZONE --format="get(networkInterfaces[0].accessConfigs[0].natIP)")
 
-sleep 20
+sleep 40
 
 
 
@@ -262,7 +262,7 @@ curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Co
   "https://compute.googleapis.com/compute/v1/projects/$DEVSHELL_PROJECT_ID/global/securityPolicies"
 
 
-sleep 20
+sleep 40
 
 curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" \
   -d "{
