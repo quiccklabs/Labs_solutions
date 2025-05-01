cf4e2e66dd16742510a352430923b76af611d728
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-05-02 00:50:34 +0530

    Update quicklabgsp767.sh

--- Exploring Cost-optimization for GKE Virtual Machines/quicklabgsp767.sh
+++ Exploring Cost-optimization for GKE Virtual Machines/quicklabgsp767.sh
@@ -1,12 +1,15 @@
 
 
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 
 
+PROJECT_ID=`gcloud config get-value project`
 
-
-
-
-export REGION="${ZONE%-*}"
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
 
 
 gcloud container clusters get-credentials hello-demo-cluster --zone $ZONE
@@ -16,15 +19,30 @@ kubectl scale deployment hello-server --replicas=2
 gcloud container clusters resize hello-demo-cluster --node-pool my-node-pool \
     --num-nodes 3 --zone $ZONE --quiet
 
+
 gcloud container node-pools create larger-pool \
   --cluster=hello-demo-cluster \
   --machine-type=e2-standard-2 \
   --num-nodes=1 \
   --zone=$ZONE
 
+
+for node in $(kubectl get nodes -l cloud.google.com/gke-nodepool=my-node-pool -o=name); do
+  kubectl cordon "$node";
+done
+
+for node in $(kubectl get nodes -l cloud.google.com/gke-nodepool=my-node-pool -o=name); do
+  kubectl drain --force --ignore-daemonsets --delete-local-data --grace-period=10 "$node";
+done
+
+kubectl get pods -o=wide
+
 gcloud container node-pools delete my-node-pool --cluster hello-demo-cluster --zone $ZONE --quiet
 
+sleep 20
+
 gcloud container clusters create regional-demo --region=$REGION --num-nodes=1
+
 
 cat << EOF > pod-1.yaml
 apiVersion: v1
@@ -64,18 +82,16 @@ EOF
 
 kubectl apply -f pod-2.yaml
 
-sleep 30
-
 kubectl get pod pod-1 pod-2 --output wide
 
 
 
+gcloud compute networks subnets update default \
+  --region=$REGION \
+  --enable-flow-logs
 
 
+bq mk --dataset --location=us ${DEVSHELL_PROJECT_ID}:us_flow_logs
 
-
-
-
-
-
+echo "Your Region :- " $REGION
 
