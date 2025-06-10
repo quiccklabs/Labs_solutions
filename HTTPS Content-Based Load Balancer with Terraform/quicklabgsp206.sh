3c9b1a9430e6c088da3900944d6a486e83431747
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-06-11 00:43:53 +0530

    Update quicklabgsp206.sh

--- HTTPS Content-Based Load Balancer with Terraform/quicklabgsp206.sh
+++ HTTPS Content-Based Load Balancer with Terraform/quicklabgsp206.sh
@@ -1,13 +1,42 @@
 
-gcloud auth list
+echo ""
+echo ""
 
-git clone https://github.com/GoogleCloudPlatform/terraform-google-lb-http.git
+read -p "Enter Group1_Region :- " group1_region
+read -p "Enter Group2_Region :- " group2_region
+read -p "Enter Group3_Region :- " group3_region
+
+
+git clone https://github.com/terraform-google-modules/terraform-google-lb-http.git
 
 cd ~/terraform-google-lb-http/examples/multi-backend-multi-mig-bucket-https-lb
 
 rm -rf main.tf
 
 wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/HTTPS%20Content-Based%20Load%20Balancer%20with%20Terraform/main.tf
+
+
+cat > variables.tf <<EOF_END
+variable "group1_region" {
+  default = "$group1_region"
+}
+
+variable "group2_region" {
+  default = "$group2_region"
+}
+
+variable "group3_region" {
+  default = "$group3_region"
+}
+
+variable "network_name" {
+  default = "ml-bk-ml-mig-bkt-s-lb"
+}
+
+variable "project" {
+  type = string
+}
+EOF_END
 
 
 terraform init 
