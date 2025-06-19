6e3c8b5eb7d10b18505e07cf167538bf4731478b
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-06-19 23:49:34 +0530

    Create quicklabgsp769.sh

---
+++ GKE Workload Optimization/quicklabgsp769.sh
@@ -0,0 +1,231 @@
+
+#!/bin/bash
+
+# Fetch zone and region
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+
+
+gcloud auth list
+
+gcloud config set project $DEVSHELL_PROJECT_ID
+
+gcloud config set compute/zone "$ZONE"
+gcloud config set compute/region "$REGION"
+
+gcloud container clusters create test-cluster --num-nodes=3  --enable-ip-alias 
+
+cat << EOF > gb_frontend_pod.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    app: gb-frontend
+  name: gb-frontend
+spec:
+    containers:
+    - name: gb-frontend
+      image: gcr.io/google-samples/gb-frontend-amd64:v5
+      resources:
+        requests:
+          cpu: 100m
+          memory: 256Mi
+      ports:
+      - containerPort: 80
+EOF
+
+kubectl apply -f gb_frontend_pod.yaml
+
+cat << EOF > gb_frontend_cluster_ip.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  name: gb-frontend-svc
+  annotations:
+    cloud.google.com/neg: '{"ingress": true}'
+spec:
+  type: ClusterIP
+  selector:
+    app: gb-frontend
+  ports:
+  - port: 80
+    protocol: TCP
+    targetPort: 80
+EOF
+
+kubectl apply -f gb_frontend_cluster_ip.yaml
+cat << EOF > gb_frontend_ingress.yaml
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: gb-frontend-ingress
+spec:
+  defaultBackend:
+    service:
+      name: gb-frontend-svc
+      port:
+        number: 80
+EOF
+
+kubectl apply -f gb_frontend_ingress.yaml
+
+sleep 600
+
+BACKEND_SERVICE=$(gcloud compute backend-services list | grep NAME | cut -d ' ' -f2)
+gcloud compute backend-services get-health $BACKEND_SERVICE --global
+
+BACKEND_SERVICE=$(gcloud compute backend-services list | grep NAME | cut -d ' ' -f2)
+gcloud compute backend-services get-health $BACKEND_SERVICE --global
+
+kubectl get ingress gb-frontend-ingress
+
+
+## Part 2
+
+
+while true; do
+    echo -ne "\e[1;93mDo you Want to proceed? (Y/n): \e[0m"
+    read confirm
+    case "$confirm" in
+        [Yy]) 
+            echo -e "\e[34mRunning the command...\e[0m"
+            break
+            ;;
+        [Nn]|"") 
+            echo "Operation canceled."
+            break
+            ;;
+        *) 
+            echo -e "\e[31mInvalid input. Please enter Y or N.\e[0m" 
+            ;;
+    esac
+done
+
+
+gsutil -m cp -r gs://spls/gsp769/locust-image .
+
+gcloud builds submit \
+    --tag gcr.io/${GOOGLE_CLOUD_PROJECT}/locust-tasks:latest locust-image
+
+gsutil cp gs://spls/gsp769/locust_deploy_v2.yaml .
+sed 's/${GOOGLE_CLOUD_PROJECT}/'$GOOGLE_CLOUD_PROJECT'/g' locust_deploy_v2.yaml | kubectl apply -f -
+
+kubectl get service locust-main
+
+cat > liveness-demo.yaml <<EOF_END
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    demo: liveness-probe
+  name: liveness-demo-pod
+spec:
+  containers:
+  - name: liveness-demo-pod
+    image: centos
+    args:
+    - /bin/sh
+    - -c
+    - touch /tmp/alive; sleep infinity
+    livenessProbe:
+      exec:
+        command:
+        - cat
+        - /tmp/alive
+      initialDelaySeconds: 5
+      periodSeconds: 10
+EOF_END
+
+kubectl apply -f liveness-demo.yaml
+
+kubectl describe pod liveness-demo-pod
+
+kubectl exec liveness-demo-pod -- rm /tmp/alive
+
+kubectl describe pod liveness-demo-pod
+
+cat << EOF > readiness-demo.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    demo: readiness-probe
+  name: readiness-demo-pod
+spec:
+  containers:
+  - name: readiness-demo-pod
+    image: nginx
+    ports:
+    - containerPort: 80
+    readinessProbe:
+      exec:
+        command:
+        - cat
+        - /tmp/healthz
+      initialDelaySeconds: 5
+      periodSeconds: 5
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: readiness-demo-svc
+  labels:
+    demo: readiness-probe
+spec:
+  type: LoadBalancer
+  ports:
+    - port: 80
+      targetPort: 80
+      protocol: TCP
+  selector:
+    demo: readiness-probe
+EOF
+
+kubectl apply -f readiness-demo.yaml
+
+kubectl get service readiness-demo-svc
+
+kubectl describe pod readiness-demo-pod
+
+sleep 45
+
+kubectl exec readiness-demo-pod -- touch /tmp/healthz
+
+kubectl describe pod readiness-demo-pod | grep ^Conditions -A 5
+
+kubectl delete pod gb-frontend
+
+cat << EOF > gb_frontend_deployment.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: gb-frontend
+  labels:
+    run: gb-frontend
+spec:
+  replicas: 5
+  selector:
+    matchLabels:
+      run: gb-frontend
+  template:
+    metadata:
+      labels:
+        run: gb-frontend
+    spec:
+      containers:
+        - name: gb-frontend
+          image: gcr.io/google-samples/gb-frontend-amd64:v5
+          resources:
+            requests:
+              cpu: 100m
+              memory: 128Mi
+          ports:
+            - containerPort: 80
+              protocol: TCP
+EOF
+
+kubectl apply -f gb_frontend_deployment.yaml
