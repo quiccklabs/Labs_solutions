a0195f294b9ee15dfc34b8d8eecdbfa91d8351ec
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-07-19 15:32:41 +0530

    Update task2.sh

--- Cloud Run Canary Deployments/task2.sh
+++ Cloud Run Canary Deployments/task2.sh
@@ -1,4 +1,11 @@
 
+export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+gcloud config set compute/region $REGION
+
+
 gh repo create cloudrun-progression --private 
 
 git clone https://github.com/GoogleCloudPlatform/training-data-analyst
