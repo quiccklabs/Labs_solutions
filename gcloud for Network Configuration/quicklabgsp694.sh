0220c79acc9830ab467fafe0baf695ded58fde04
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-07-21 20:56:02 +0530

    Create quicklabgsp694.sh

---
+++ gcloud for Network Configuration/quicklabgsp694.sh
@@ -0,0 +1,12 @@
+
+gcloud compute firewall-rules create labnet-allow-internal \
+	--network=labnet \
+	--action=ALLOW \
+	--rules=icmp,tcp:22 \
+	--source-ranges=0.0.0.0/0
+
+gcloud compute firewall-rules create privatenet-deny \
+    --network=privatenet \
+    --action=DENY \
+    --rules=icmp,tcp:22 \
+    --source-ranges=0.0.0.0/0
