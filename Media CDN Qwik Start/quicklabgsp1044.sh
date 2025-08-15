0181317f737b8ba105d25312601563d99e7b463e
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-08-15 23:19:19 +0530

    Create quicklabgsp1044.sh

---
+++ Media CDN Qwik Start/quicklabgsp1044.sh
@@ -0,0 +1,23 @@
+
+
+#!/bin/bash
+
+# Fetch zone and region
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+
+
+gcloud services enable networkservices.googleapis.com
+
+# 1. Create the bucket
+gsutil mb gs://$PROJECT_ID/
+
+# 2. Remove Public Access Prevention
+gsutil pap set unspecified gs://$PROJECT_ID/
+
+# 3. Grant public read access to objects in the bucket
+gsutil iam ch allUsers:objectViewer gs://$PROJECT_ID/
