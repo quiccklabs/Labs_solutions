3ab492da970b20f24abe8acfe11bb01a51fde871
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-08-21 21:54:26 +0530

    Update quicklabgsp021.sh

--- Orchestrating the Cloud with Kubernetes/quicklabgsp021.sh
+++ Orchestrating the Cloud with Kubernetes/quicklabgsp021.sh
@@ -1,20 +1,26 @@
+
+
+
+
+
+# Fetch zone and region
 ZONE=$(gcloud compute project-info describe \
   --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 REGION=$(gcloud compute project-info describe \
   --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 PROJECT_ID=$(gcloud config get-value project)
 
+
+
 gcloud config set compute/zone $ZONE
 
-gcloud container clusters create io
+gcloud container clusters create io --zone $ZONE
 
-git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+gcloud storage cp -r gs://spls/gsp021/* .
 
-ln -s ~/training-data-analyst/courses/ak8s/CloudBridge ~/ak8s
+cd orchestrate-with-kubernetes/kubernetes
 
-cd ~/ak8s/
-
-kubectl create deployment nginx --image=nginx:1.10.0
+kubectl create deployment nginx --image=nginx:1.27.0
 
 kubectl get pods
 
@@ -22,46 +28,37 @@ kubectl expose deployment nginx --port 80 --type LoadBalancer
 
 kubectl get services
 
-cd ~/ak8s
+cd ~/orchestrate-with-kubernetes/kubernetes
 
-kubectl create -f pods/monolith.yaml
-
-kubectl get pods
-
-cd ~/ak8s
-
-kubectl create secret generic tls-certs --from-file tls/
-kubectl create configmap nginx-proxy-conf --from-file nginx/proxy.conf
-kubectl create -f pods/secure-monolith.yaml
-
-kubectl create -f services/monolith.yaml
-
-gcloud compute firewall-rules create allow-monolith-nodeport \
-  --allow=tcp:31000
+kubectl create -f pods/fortune-app.yaml
 
 
-kubectl get pods -l "app=monolith"
+kubectl create secret generic tls-certs --from-file tls/  
+kubectl create configmap nginx-proxy-conf --from-file nginx/proxy.conf  
+kubectl create -f pods/secure-fortune.yaml
 
+kubectl create -f services/fortune-app.yaml
 
-kubectl get pods -l "app=monolith,secure=enabled"
+gcloud compute firewall-rules create allow-fortune-nodeport --allow tcp:31000
 
+kubectl get pods -l "app=fortune-app"
 
-kubectl label pods secure-monolith 'secure=enabled'
-kubectl get pods secure-monolith --show-labels
+kubectl get pods -l "app=fortune-app,secure=enabled"
 
-kubectl describe services monolith | grep Endpoints
+kubectl label pods secure-fortune 'secure=enabled'
+kubectl get pods secure-fortune --show-labels
 
 
 kubectl create -f deployments/auth.yaml
 
 kubectl create -f services/auth.yaml
 
-kubectl create -f deployments/hello.yaml
-kubectl create -f services/hello.yaml
+kubectl create -f deployments/fortune-service.yaml
+kubectl create -f services/fortune-service.yaml
 
-kubectl create configmap nginx-frontend-conf --from-file=nginx/frontend.conf
-kubectl create -f deployments/frontend.yaml
+
+kubectl create configmap nginx-frontend-conf --from-file=nginx/frontend.conf  
+kubectl create -f deployments/frontend.yaml  
 kubectl create -f services/frontend.yaml
-
 
 kubectl get services frontend
