6bd5e4bfe6812f155ed51f68912a49d8cae127af
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2025-09-22 19:40:17 +0530

    Update task3.sh

--- Deploy a Hugo Website with Cloud Build and Firebase Pipeline/task3.sh
+++ Deploy a Hugo Website with Cloud Build and Firebase Pipeline/task3.sh
@@ -27,3 +27,23 @@ gcloud builds list --region=$REGION
 gcloud builds log --region=$REGION $(gcloud builds list --format='value(ID)' --filter=$(git rev-parse HEAD) --region=$REGION)
 
 gcloud builds log "$(gcloud builds list --format='value(ID)' --filter=$(git rev-parse HEAD) --region=$REGION)" --region=$REGION | grep "Hosting URL"
+
+
+
+sleep 20
+
+
+sed -i "s/Blogging with Hugo and Cloud Build/logging with Hugo and Cloud Build/g" config.toml
+
+git add .
+git commit -m "I updated the site title"
+git push -u origin main
+
+gcloud builds list --region=$REGION
+
+gcloud builds log --region=$REGION $(gcloud builds list --format='value(ID)' --filter=$(git rev-parse HEAD) --region=$REGION)
+
+gcloud builds log "$(gcloud builds list --format='value(ID)' --filter=$(git rev-parse HEAD) --region=$REGION)" --region=$REGION | grep "Hosting URL"
+
+
+
