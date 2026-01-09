81455b3fc5c42eac318a1e30e1d3a7ff0e4ef494
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2026-01-09 18:39:14 +0530

    Add metadata fetching for zone and region

--- App Engine Qwik Start Java/quicklabgsp068.sh
+++ App Engine Qwik Start Java/quicklabgsp068.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+
+# Fetch zone and region
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+
 
 gcloud services enable appengine.googleapis.com
 
