676b5888cb4f27b9ae943a58f8aaa5ac2f81fc12
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2026-03-20 17:19:21 +0530

    Add files via upload

--- Automating the Deployment of Infrastructure Using Terraform/mynetwork.tf
+++ Automating the Deployment of Infrastructure Using Terraform/mynetwork.tf
@@ -1,39 +1,34 @@
 # Create the mynetwork network
 resource "google_compute_network" "mynetwork" {
-  name                    = "mynetwork"
-  auto_create_subnetworks = true
+  name = "mynetwork"
+  # RESOURCE properties go here
+  auto_create_subnetworks = "true"
 }
-
-# Create a firewall rule to allow HTTP, SSH, RDP and ICMP traffic on mynetwork
+# Add a firewall rule to allow HTTP, SSH, RDP and ICMP traffic on mynetwork
 resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
-  name    = "mynetwork-allow-http-ssh-rdp-icmp"
+  name = "mynetwork-allow-http-ssh-rdp-icmp"
+  # RESOURCE properties go here
   network = google_compute_network.mynetwork.self_link
-  source_ranges = [
-    "0.0.0.0/0"
-  ]
-
   allow {
     protocol = "tcp"
     ports    = ["22", "80", "3389"]
   }
-
   allow {
     protocol = "icmp"
   }
+  source_ranges = ["0.0.0.0/0"]
 }
-
-# Create the mynet-us-vm instance
-module "mynet-us-vm" {
-  source              = "./instance"
-  instance_name       = "mynet-us-vm"
-  instance_zone       = "us-central1-a"
-  instance_subnetwork = google_compute_network.mynetwork.self_link
+# Create the mynet-vm-1 instance
+module "mynet-vm-1" {
+  source           = "./instance"
+  instance_name    = "mynet-vm-1"
+  instance_zone    = "$ZONE"
+  instance_network = google_compute_network.mynetwork.self_link
 }
-
-# Create the mynet-eu-vm" instance
-module "mynet-eu-vm" {
-  source              = "./instance"
-  instance_name       = "mynet-eu-vm"
-  instance_zone       = "europe-west1-d"
-  instance_subnetwork = google_compute_network.mynetwork.self_link
+# Create the mynet-vm-2" instance
+module "mynet-vm-2" {
+  source           = "./instance"
+  instance_name    = "mynet-vm-2"
+  instance_zone    = "$ZONE_2"
+  instance_network = google_compute_network.mynetwork.self_link
 }
