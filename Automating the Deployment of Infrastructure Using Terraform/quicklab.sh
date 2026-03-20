f2329e8511531e8913fd8415c6cff825c578241d
Quicklab☁️ <108678347+quiccklabs@users.noreply.github.com>
2026-03-20 17:41:28 +0530

    Refactor quicklab.sh to use zone variables
    Added zone variables and updated Terraform commands to use them.

--- Automating the Deployment of Infrastructure Using Terraform/quicklab.sh
+++ Automating the Deployment of Infrastructure Using Terraform/quicklab.sh
@@ -1,26 +1,35 @@
 
 
-gcloud auth list
+export ZONE_1=us-central1-c
 
-mkdir tfinfra
+export ZONE_2=us-west1-b
 
-cd tfinfra
+
+read -p "Enter ZONE 1:- " ZONE_1
+read -p "Enter ZONE 2:- " ZONE_2
+
+# wget command
+
 
 
 wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/mynetwork.tf
 
 wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/provider.tf
 
-wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/terraform.tfstate
 
-wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/variables.tf
-
+sed -i "s/instance_zone    = \"\$ZONE\"/instance_zone    = \"$ZONE_1\"/g" mynetwork.tf
+sed -i "s/instance_zone    = \"\$ZONE_2\"/instance_zone    = \"$ZONE_2\"/g" mynetwork.tf
 
 mkdir instance
 
 cd instance
 
 wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/instance/main.tf
+
+wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/refs/heads/master/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/instance/variables.tf
+
+
+
 
 cd ..
 
@@ -30,6 +39,9 @@ terraform fmt
 
 terraform init
 
-echo -e "mynet-us-vm\nmynetwork\n$ZONE" | terraform plan -var="instance_name=$(</dev/stdin)" -var="instance_network=$(</dev/stdin)" -var="instance_zone=$(</dev/stdin)"
+terraform plan
 
-echo -e "mynet-us-vm\nmynetwork\n$ZONE" | terraform apply -var="instance_name=$(</dev/stdin)" -var="instance_network=$(</dev/stdin)" -var="instance_zone=$(</dev/stdin)" --auto-approve
+terraform apply --auto-approve
+
+
+
